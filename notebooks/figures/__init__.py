479d56fae22b6d9f6d6bce6d6ba2c3510f53b999
Andreas Mueller <amueller@nyu.edu>
2015-06-16 17:46:46 -0400

    Move / add text from 6.3 to 6.2, make 6.3 be learning curves and validation corves, make 6.4 be model evaluation and scoring

--- notebooks/figures/__init__.py
+++ notebooks/figures/__init__.py
@@ -3,7 +3,7 @@ from .linear_regression import plot_linear_regression
 from .ML_flow_chart import plot_supervised_chart, plot_unsupervised_chart
 from .bias_variance import plot_underfit_overfit_polynomial
 from .sdss_filters import plot_sdss_filters, plot_redshifts
-from .plot_kneighbors_regularization import plot_kneighbors_regularization
+from .plot_kneighbors_regularization import plot_kneighbors_regularization, plot_regression_datasets, make_dataset
 from .plot_linear_svc_regularization import plot_linear_svc_regularization
 from .plot_interactive_tree import plot_tree_interactive
 
@@ -11,4 +11,4 @@ __all__ = ['plot_2d_separator', 'plot_linear_regression',
            'plot_supervised_chart', 'plot_unsupervised_chart',
            'plot_underfit_overfit_polynomial', 'plot_sdss_filters', 'plot_redshifts',
            'plot_kneighbors_regularization', 'plot_linear_svc_regularization',
-           'plot_tree_interactive']
+           'plot_tree_interactive', 'plot_regression_datasets', 'make_dataset']
--- notebooks/figures/plot_kneighbors_regularization.py
+++ notebooks/figures/plot_kneighbors_regularization.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 
 
+def make_dataset(n_samples=100):
+    rnd = np.random.RandomState(42)
+    x = np.linspace(-3, 3, n_samples)
+    y_no_noise = np.sin(4 * x) + x
+    y = y_no_noise + rnd.normal(size=len(x))
+    return x, y
+
+
+def plot_regression_datasets():
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    for n_samples, ax in zip([10, 100, 1000], axes):
+        x, y = make_dataset(n_samples)
+        ax.plot(x, y, 'o', alpha=.6)
+
+
 def plot_kneighbors_regularization():
     rnd = np.random.RandomState(42)
     x = np.linspace(-3, 3, 100)

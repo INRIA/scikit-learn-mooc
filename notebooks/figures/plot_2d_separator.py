dbee43a2fa574167741b2cf558099b1d3e4676bc
Andreas Mueller <amueller@nyu.edu>
2015-06-20 17:21:39 -0400

    slight factoring of plot_2d_separator, add fill keyword

--- notebooks/figures/plot_2d_separator.py
+++ notebooks/figures/plot_2d_separator.py
@@ -2,13 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_2d_decision_function(classifier, X):
-    pass
-
-
-def plot_2d_separator(classifier, X):
-    x_min, x_max = X[:, 0].min(), X[:, 0].max()
-    y_min, y_max = X[:, 1].min(), X[:, 1].max()
+def plot_2d_separator(classifier, X, fill=False):
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
     xx = np.linspace(x_min, x_max, 10)
     yy = np.linspace(y_min, y_max, 10)
 
@@ -19,13 +15,15 @@ def plot_2d_separator(classifier, X):
     except AttributeError:
         # no decision_function
         decision_values = classifier.predict_proba(X_grid)[:, 0]
-    levels = [0.0]
-    linestyles = ['solid']
-    colors = 'k'
 
     ax = plt.axes()
-    ax.contour(X1, X2, decision_values.reshape(X1.shape), levels, colors=colors,
-               linestyles=linestyles)
+    if fill:
+        ax.contourf(X1, X2, decision_values.reshape(X1.shape))
+    else:
+        ax.contour(X1, X2, decision_values.reshape(X1.shape), levels=[0],
+                   colors="black")
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
 
 
 if __name__ == '__main__':
@@ -33,6 +31,6 @@ if __name__ == '__main__':
     from sklearn.linear_model import LogisticRegression
     X, y = make_blobs(centers=2, random_state=42)
     clf = LogisticRegression().fit(X, y)
-    plot_2d_separator(clf, X)
+    plot_2d_separator(clf, X, fill=True)
     plt.scatter(X[:, 0], X[:, 1], c=y)
     plt.show()

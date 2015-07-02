3424cd60cf8dee6f4ce38c85fd2521539a9e65b6
Andreas Mueller <amueller@nyu.edu>
2015-07-02 18:23:17 -0400

    don't fetch 20 news which we don't use anywhere.

--- fetch_data.py
+++ fetch_data.py
@@ -4,16 +4,8 @@ try:
 except ImportError:
     from urllib import urlopen
 
-import tarfile
 import zipfile
 
-
-TWENTY_URL = ("http://people.csail.mit.edu/jrennie/"
-              "20Newsgroups/20news-bydate.tar.gz")
-TWENTY_ARCHIVE_NAME = "20news-bydate.tar.gz"
-TWENTY_CACHE_NAME = "20news-bydate.pkz"
-TWENTY_TRAIN_FOLDER = "20news-bydate-train"
-TWENTY_TEST_FOLDER = "20news-bydate-test"
 
 SENTIMENT140_URL = ("http://cs.stanford.edu/people/alecmgo/"
                     "trainingandtestdata.zip")
@@ -38,30 +30,6 @@ def get_datasets_folder():
     else:
         print("Using existing dataset folder:" + datasets_folder)
     return datasets_folder
-
-
-def check_twenty_newsgroups(datasets_folder):
-    print("Checking availability of the 20 newsgroups dataset")
-
-    archive_path = os.path.join(datasets_folder, TWENTY_ARCHIVE_NAME)
-    train_path = os.path.join(datasets_folder, TWENTY_TRAIN_FOLDER)
-    test_path = os.path.join(datasets_folder, TWENTY_TEST_FOLDER)
-
-    if not os.path.exists(archive_path):
-        print("Downloading dataset from %s (14 MB)" % TWENTY_URL)
-        opener = urlopen(TWENTY_URL)
-        open(archive_path, 'wb').write(opener.read())
-    else:
-        print("Found archive: " + archive_path)
-
-    if not os.path.exists(train_path) or not os.path.exists(test_path):
-        print("Decompressing %s" % archive_path)
-        tarfile.open(archive_path, "r:gz").extractall(path=datasets_folder)
-
-    print("Checking that the 20 newsgroups files exist...")
-    assert os.path.exists(train_path)
-    assert os.path.exists(test_path)
-    print("=> Success!")
 
 
 def check_sentiment140(datasets_folder):
@@ -92,7 +60,6 @@ def check_sentiment140(datasets_folder):
 
 if __name__ == "__main__":
     datasets_folder = get_datasets_folder()
-    check_twenty_newsgroups(datasets_folder)
     check_sentiment140(datasets_folder)
 
     print("Loading Labeled Faces Data (~200MB)")

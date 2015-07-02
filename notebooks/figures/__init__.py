1e7f63d5f04f816403b2e6d0223ad5073497c78e
Andreas Mueller <amueller@nyu.edu>
2015-07-02 17:35:52 -0400

    remove some unnecessary plotting code

--- notebooks/figures/__init__.py
+++ notebooks/figures/__init__.py
@@ -1,8 +1,4 @@
 from .plot_2d_separator import plot_2d_separator
-from .linear_regression import plot_linear_regression
-from .ML_flow_chart import plot_supervised_chart, plot_unsupervised_chart
-from .bias_variance import plot_underfit_overfit_polynomial
-from .sdss_filters import plot_sdss_filters, plot_redshifts
 from .plot_kneighbors_regularization import plot_kneighbors_regularization, \
     plot_regression_datasets, make_dataset
 from .plot_linear_svc_regularization import plot_linear_svc_regularization
@@ -11,10 +7,8 @@ from .plot_interactive_forest import plot_forest_interactive
 from .plot_rbf_svm_parameters import plot_rbf_svm_parameters
 from .plot_rbf_svm_parameters import plot_svm_interactive
 
-__all__ = ['plot_2d_separator', 'plot_linear_regression',
-           'plot_supervised_chart', 'plot_unsupervised_chart',
-           'plot_underfit_overfit_polynomial', 'plot_sdss_filters', 'plot_redshifts',
-           'plot_kneighbors_regularization', 'plot_linear_svc_regularization',
-           'plot_tree_interactive', 'plot_regression_datasets', 'make_dataset',
+__all__ = ['plot_2d_separator', 'plot_kneighbors_regularization',
+           'plot_linear_svc_regularization', 'plot_tree_interactive',
+           'plot_regression_datasets', 'make_dataset',
            "plot_forest_interactive", "plot_rbf_svm_parameters",
            "plot_svm_interactive"]

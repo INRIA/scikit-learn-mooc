2c514c720377614b8d274fc919e6f1f20b08d461
Shaun Walbridge <shaun.walbridge@gmail.com>
2015-07-05 19:42:56 -0400

    Update check_env.py
    An improved script which prints out the packages found and their versions in the success case for each. The current script has no output in a success case (all dependencies found).

--- check_env.py
+++ check_env.py
@@ -1,27 +1,50 @@
-from distutils.version import LooseVersion as Version
+rom __future__ import print_function
+
+import importlib
 import sys
-import numpy
-import matplotlib
-import scipy
-import IPython
-import sklearn
+
+from distutils.version import LooseVersion as Version
+
+
+def import_version(pkg, min_ver):
+    mod = None
+    try:
+        mod = importlib.import_module(pkg)
+        if Version(mod.__version__) < min_ver:
+            print(FAIL, "%s version %s or higher required, but %s installed." % (lib, min_ver, mod.__version__))
+        else:
+            print(OK, '%s version %s' % (pkg, mod.__version__))
+    except ImportError:
+        print(FAIL, '%s not installed' % pkg)
+    return mod
+
+try:
+    import curses
+    curses.setupterm()
+    assert curses.tigetnum("colors") > 2
+    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
+    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
+except:
+    OK = '[ OK ]'
+    FAIL = '[FAIL]'
 
 # first check the python version
+print('Using python in', sys.prefix)
+print(sys.version)
 pyversion = Version(sys.version)
 if pyversion >= "3":
     if pyversion < "3.4":
-        print("ERROR: Python version 3.4 (or 2.7) is required, but %s is installed." % sys.version)
+        print(FAIL, "Python version 3.4 (or 2.7) is required, but %s is installed." % sys.version)
 elif pyversion >= "2":
     if pyversion < "2.7":
-        print("ERROR: Python version 2.7 is required, but %s is installed." % sys.version)
+        print(FAIL, "Python version 2.7 is required, but %s is installed." % sys.version)
 else:
-    print("ERROR: Unknown Python version: %s" % sys.version)
+    print(FAIL, "Unknown Python version: %s" % sys.version)
 
+print()
+requirements = {'numpy': "1.6.1", 'scipy': "0.9", 'matplotlib': "1.0",
+                'IPython': "3.0", 'sklearn': "0.15"}
 
-requirements = {numpy: "1.6.1", scipy: "0.9", matplotlib: "1.0", IPython: "3.0",
-                sklearn: "0.15"}
-
-for lib, required_version in requirements.items():
-    if Version(lib.__version__) < required_version:
-        print("ERROR: %s version %s or higher required, but %s installed."
-              % (lib.__name__, required_version, lib.__version__))
+# now the dependencies
+for lib, required_version in list(requirements.items()):
+    import_version(lib, required_version)

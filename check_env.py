c21573aa89a5b3b4964568085a4d6e10bcea2f6f
Kyle Kastner <kastnerkyle@gmail.com>
2015-07-06 00:19:38 -0400

    Two fixes to check_env script

--- check_env.py
+++ check_env.py
@@ -1,8 +1,20 @@
-rom __future__ import print_function
+from __future__ import print_function
+try:
+    import curses
+    curses.setupterm()
+    assert curses.tigetnum("colors") > 2
+    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
+    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
+except:
+    OK = '[ OK ]'
+    FAIL = '[FAIL]'
 
-import importlib
+
 import sys
-
+try:
+    import importlib
+except ImportError:
+    print(FAIL, "Python version 3.4 (or 2.7) is required, but %s is installed." % sys.version)
 from distutils.version import LooseVersion as Version
 
 
@@ -18,15 +30,6 @@ def import_version(pkg, min_ver):
         print(FAIL, '%s not installed' % pkg)
     return mod
 
-try:
-    import curses
-    curses.setupterm()
-    assert curses.tigetnum("colors") > 2
-    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
-    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
-except:
-    OK = '[ OK ]'
-    FAIL = '[FAIL]'
 
 # first check the python version
 print('Using python in', sys.prefix)

078de995f2bb5f6cb526c601edbc7a768b97647d
Olivier Grisel <olivier.grisel@ensta.org>
2019-08-21 14:24:30 +0200

    Use Adult Census data for the preprocessing notebook

--- python_scripts/xx_basic_preprocessing.py
+++ python_scripts/xx_basic_preprocessing.py
@@ -17,47 +17,58 @@
 # %% [markdown]
 # # Introduction to scikit-learn: basic preprocessing for basic model fitting
 #
-# In this lecture note, we will aim at introducing:
+# In this notebook, we will aim at introducing a typical approach to build
+# predictive models on tabular datasets.
+#
+# In particular we will highlight:
 # * the difference between numerical and categorical variables;
 # * the importance of scaling numerical variables;
-# * the way to encode categorical variables;
-# * combine different preprocessing on different type of data;
+# * typical ways to deal categorical variables;
+# * train predictive models on different kinds of data;
 # * evaluate the performance of a model via cross-validation.
 #
 # ## Introduce the dataset
 #
-# To this aim, we will use data from the 1985 "Current Population Survey"
-# (CPS). The goal with this data is to regress wages from heterogeneous data
-# such as age, experience, education, family information, etc.
+# To this aim, we will use data from the 1994 Census bureau database. The goal
+# with this data is to regress wages from heterogeneous data such as age,
+# employment, education, family information, etc.
 #
 # Let's first load the data located in the `datasets` folder.
 
 # %%
-import os
-import time
 import pandas as pd
 
-df = pd.read_csv(os.path.join('datasets', 'cps_85_wages.csv'))
+df = pd.read_csv('datasets/adult-census.csv')
+# df = pd.read_csv("https://www.openml.org/data/get_csv/1595261/adult-census.csv")
 
 # %% [markdown]
-# We can quickly have a look at the head of the dataframe to check the type
-# of available data.
+#
+# Let's have a look at the first records of this data frame:
 
 # %%
-print(df.head())
+df.head()
 
 # %% [markdown]
-# The target in our study will be the "WAGE" columns while we will use the
-# other columns to fit a model
+#
+# The target variable in our study will be the "class" column while we will use
+# the other columns as input variables for our model. This target column divides
+# the samples (also known as records) into two groups: high income (>50K) vs low
+# income (<=50K). The resulting prediction problem is therefore a binary
+# classification problem.
+#
+# For simplicity, we will ignore the "fnlwgt" (final weight) column that was
+# crafted by the creators of the dataset when sampling the dataset to be
+# representative of the full census database.
 
 # %%
-target_name = "WAGE"
+target_name = "class"
 target = df[target_name].to_numpy()
-data = df.drop(columns=target_name)
+data = df.drop(columns=[target_name, "fnlwgt"])
 
 # %% [markdown]
+#
 # We can check the number of samples and the number of features available in
-# the dataset
+# the dataset:
 
 # %%
 print(
@@ -66,22 +77,27 @@ print(
 )
 
 # %% [markdown]
-# ## Work with numerical data
 #
-# The most intuitive type of data in machine learning which can (almost)
-# directly be used in machine learning are known as numerical data. We can
-# quickly have a look at such data by selecting the subset of columns from
-# the original data.
+# ## Working with numerical data
+#
+# The numerical data is the most natural type of data used in machine learning
+# and can (almost) directly be fed to predictive models. We can quickly have a
+# look at such data by selecting the subset of columns from the original data.
+#
+# We will use this subset of data to fit a linear classification model to
+# predict the income class.
 
 # %%
-print(data.columns)
-numerical_columns = ['AGE', 'EDUCATION', 'EXPERIENCE']
-
-# %% [markdown]
-# We will use this subset of data to fit linear regressor to infer the wage
+data.columns
 
 # %%
+data.dtypes
+
+# %%
+numerical_columns = ['age', 'education-num', 'hours-per-week',
+                     'capital-gain', 'capital-loss']
 data_numeric = data[numerical_columns]
+data_numeric.head()
 
 # %% [markdown]
 # When building a machine learning model, it is important to leave out a
@@ -91,7 +107,7 @@ data_numeric = data[numerical_columns]
 #
 # Scikit-learn provides an helper function `train_test_split` which will
 # split the dataset into a training and a testing set. It will ensure that
-# the data are shuffled before splitting the data.
+# the data are shuffled randomly before splitting the data.
 
 # %%
 from sklearn.model_selection import train_test_split
@@ -109,90 +125,135 @@ print(
     f"{data_test.shape[1]} features"
 )
 
-# %% [markdown]
-# We will build a Support Vector Machine (SVM) which is a linear model. The
-# `fit` method is called to train the data and only the training data should
-# be given for this purpose.
-# To evaluate our model, we can use the method `score`. It will compute the
-# coefficient of determination R2 when dealing with a regression problem.
+# %% [markdown] We will build a linear classification model called "Logistic
+# Regression". The `fit` method is called to train the data and only the
+# training data should be given for this purpose.
 #
 # In addition, we checking the time required to train the model and internally
 # check the number of iterations done by the solver to find a solution.
 # %%
-from sklearn.svm import LinearSVR
+from sklearn.linear_model import LogisticRegression
+import time
 
-model = LinearSVR()
+model = LogisticRegression()
 start = time.time()
 model.fit(data_train, target_train)
 elapsed_time = time.time() - start
+
 print(
-    f"The R2 score using a {model.__class__.__name__} is "
-    f"{model.score(data_test, target_test):.2f} with a fitting time of "
-    f"{elapsed_time:.3f} seconds in {model.n_iter_} iterations"
+    f"The model {model.__class__.__name__} was trained in "
+    f"{elapsed_time:.3f} seconds for {model.n_iter_} iterations"
 )
 
 # %% [markdown]
-# We should not the `ConvergenceWarning` which inform us that our model stopped
-# learning since it reaches the maximum number of iterations allowed by the
-# user. This could potentially be detrimental for the model accuracy. We can
-# follow the (bad) advice given in the warning message and increase the maximum
-# number of iterations allowed.
+#
+# Let's ignore the convergence warning for now and instead let's try
+# to use our model to make some predictions on the first three records
+# of the held out test set:
 
 # %%
-model = LinearSVR(max_iter=50000)
+
+data_test[:3]
+
+# %%
+
+model.predict(data_test[:3])
+
+# %%
+
+target_test[:3]
+
+# %% [markdown]
+#
+# To quantitatively evaluate our model, we can use the method `score`. It will
+# compute the classification accuracy when dealing with a classificiation
+# problem.
+
+print(
+    f"The test accuracy using a {model.__class__.__name__} is "
+    f"{model.score(data_test, target_test):.3f}"
+)
+
+# %% [markdown]
+#
+# Let's now consider the `ConvergenceWarning` message that was raised previously
+# when calling the `fit` method to train our model. This warning informs us that
+# our model stopped learning becaused it reached the maximum number of
+# iterations allowed by the user. This could potentially be detrimental for the
+# model accuracy. We can follow the (bad) advice given in the warning message
+# and increase the maximum number of iterations allowed.
+
+# %%
+model = LogisticRegression(max_iter=50000)
 start = time.time()
 model.fit(data_train, target_train)
 elapsed_time = time.time() - start
 print(
-    f"The R2 score using a {model.__class__.__name__} is "
-    f"{model.score(data_test, target_test):.2f} with a fitting time of "
+    f"The accuracy using a {model.__class__.__name__} is "
+    f"{model.score(data_test, target_test):.3f} with a fitting time of "
     f"{elapsed_time:.3f} seconds in {model.n_iter_} iterations"
 )
 
 # %% [markdown]
-# We can observe an increase in performance add the cost of a longer training.
-# Instead of increasing the number of iterations, we could instead know a bit
-# more about the SVR model and known that it is expecting input data to be
-# scaled before to start training. A range of preprocessing algorithms in
-# scikit-learn allows to transform the input data before to train a model.
-# We can easily combine these sequential operation with a scikit-learn
-# `Pipeline` which will chain the operations and can be used as any other
-# classifier or regressor. The helper function `make_pipeline` will create
-# a `Pipeline` by giving the successive transformations to perform.
 #
-# In our case, we will standardize the data and then train a linear SVR.
+# We can observe now a longer training time but not significant improvement in
+# the predictive performance. Instead of increasing the number of iterations, we
+# can try to help fit the model faster by scaling the data first. A range of
+# preprocessing algorithms in scikit-learn allows to transform the input data
+# before training a model. We can easily combine these sequential operation with
+# a scikit-learn `Pipeline` which will chain the operations and can be used as
+# any other classifier or regressor. The helper function `make_pipeline` will
+# create a `Pipeline` by giving the successive transformations to perform.
+#
+# In our case, we will standardize the data and then train a new logistic
+# regression model on that new version of the dataset set.
+
+# %%
+data_train.describe()
 
 # %%
 from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+data_train_scaled = scaler.fit_transform(data_train)
+data_train_scaled
+
+# %%
+data_train_scaled = pd.DataFrame(data_train_scaled, columns=data_train.columns)
+data_train_scaled.describe()
+
+# %%
+
 from sklearn.pipeline import make_pipeline
 
-model = make_pipeline(StandardScaler(), LinearSVR())
+model = make_pipeline(StandardScaler(), LogisticRegression())
 start = time.time()
 model.fit(data_train, target_train)
 elapsed_time = time.time() - start
 print(
-    f"The R2 score using a {model.__class__.__name__} is "
-    f"{model.score(data_test, target_test):.2f} with a fitting time of "
+    f"The accuracy using a {model.__class__.__name__} is "
+    f"{model.score(data_test, target_test):.3f} with a fitting time of "
     f"{elapsed_time:.3f} seconds in {model[-1].n_iter_} iterations"
 )
 
 # %% [markdown]
-# We can see that the training time and the number of iterations is much
-# shorter while the accuracy is equivalent.
 #
-# In the previous example, we split the original data into a training set and
-# a testing set. This strategy has several issues: in the setting where the
-# amount of data is limited, the subset of data used to train or test will be
-# small; and the splitting was done in a random manner and we have no
-# information regarding the confidence of the results obtained.
+# We can see that the training time and the number of iterations is much shorter
+# while the predictive performance is equivalent.
 #
-# Therefore, we can use what cross-validation. Cross-validation consists in
+# In the previous example, we split the original data into a training set and a
+# testing set. This strategy has several issues: in the setting where the amount
+# of data is limited, the subset of data used to train or test will be small;
+# and the splitting was done in a random manner and we have no information
+# regarding the confidence of the results obtained.
+#
+# Instead, we can use what cross-validation. Cross-validation consists in
 # repeating this random splitting into training and testing sets and aggregate
-# the model performance. By repeating the experiment, one can get a the
-# fluctuation of the model performance.
+# the model performance. By repeating the experiment, one can get an estimate of
+# the variabilty of the model performance.
 #
-# The function `cross_val_score` allows for such experimental protocol by
-# giving the model, the data and the target. Since there is several
+# The function `cross_val_score` allows for such experimental protocol by giving
+# the model, the data and the target. Since there exists several
 # cross-validation strategies, `cross_val_score` takes a parameter `cv` which
 # defines the splitting strategy.
 
@@ -200,26 +261,33 @@ print(
 from sklearn.model_selection import cross_val_score
 
 score = cross_val_score(model, data_numeric, target, cv=5)
-print(f"The R2 score (mean +/- 1 std. dev.) is: "
-      f"{score.mean():.2f} +/- {score.std():.2f}")
+print(f"The accuracy (mean +/- 1 std. dev.) is: "
+      f"{score.mean():.3f} +/- {score.std():.3f}")
 print(f"The different scores obtained are: \n{score}")
 
 # %% [markdown]
-# Setting `cv=5` created 5 training and testing sets on which we trained and
-# tested a model. This strategy is called K-fold cross-validation where `K`
-# corresponds to the number of split.
 #
+# Setting `cv=5` created 5 distinct splits to get 5 variations for the training
+# and testing sets. Each training set is used to fit one model which is then
+# scored on the matching test set. This strategy is called K-fold
+# cross-validation where `K` corresponds to the number of splits.
+
+# %%
+
+# TODO add CV diagram for 5-fold CV from the gallery
+
+# %% [markdown]
 # ## Work with categorical data
 #
-# In the previous section, we dealt with data for which numerical algorithms
-# are mathematically designed to work natively. However, real datasets contain
-# type of data which do not belong to this category and will require some
+# In the previous section, we dealt with data for which numerical algorithms are
+# mathematically designed to work natively. However, real datasets contain type
+# of data which do not belong to this category and will require some
 # preprocessing. Such preprocessing will transform these data to be numerical
 # and thus natively handled by machine learning algorithms.
 #
 # Categorical data are broadly encountered in data science. Numerical data is a
-# continuous quantity corresponding to a real numbers while categorical data
-# are represented as discrete values. For instance, the variable `SEX` in our
+# continuous quantity corresponding to a real numbers while categorical data are
+# represented as discrete values. For instance, the variable `SEX` in our
 # previous dataset is a categorical variable because it encodes the data with
 # the two categories `male` and `female`.
 #
@@ -228,8 +296,13 @@ print(f"The different scores obtained are: \n{score}")
 # machine-learning algorithm.
 
 # %%
+data.dtypes
+
+
+# %%
 categorical_columns = [
-    'SOUTH', 'SEX', 'UNION', 'RACE', 'OCCUPATION', 'SECTOR', 'MARR'
+    'workclass', 'education', 'marital-status', 'occupation', 'relationship',
+    'race', 'sex', 'native-country'
 ]
 data_categorical = data[categorical_columns]
 print(data_categorical.head())
@@ -291,47 +364,50 @@ print(data_encoded[:5])
 # machine learning pipeline using cross-validation.
 
 # %%
-model = make_pipeline(OneHotEncoder(handle_unknown='ignore'), LinearSVR())
+model = make_pipeline(OneHotEncoder(handle_unknown='ignore'),
+                      LogisticRegression(max_iter=1000))
 score = cross_val_score(model, data_categorical, target)
-print(f"The R2 score is: {score.mean():.2f} +/- {score.std():.2f}")
+print(f"The accuracy is: {score.mean():.3f} +/- {score.std():.3f}")
 print(f"The different scores obtained are: \n{score}")
 
 # %% [markdown]
-# ## Combining different preprocessing on different data type
+# ## Combining different transformers used for different column types
 #
 # In the previous section, we saw that we need to treat data specifically
-# depending of their nature (i.e. numerical or categorical). We were capable
-# of making the preprocessing in two sequential steps but we did not present
-# any tool which could allow us to first preprocess the data depending of
-# their type and later on use these preprocessed data to train a single
-# machine learning model.
+# depending of their nature (i.e. numerical or categorical) and trained one
+# pipeline (transformer + classifier) for each kind of dataset.
 #
-# Scikit-learn provides a `ColumnTransformer` which will dispatch some specific
-# columns to a specific transformer.
+# Scikit-learn provides a `ColumnTransformer` class which will dispatch some
+# specific columns to a specific transformer making it easy to fit a single
+# predictive model on a data that combines both kinds of variable together
+# (heterogeneously typed tabular data).
 #
 # We can first define the columns depending on their data type:
-# * binary encoding: it will corresponds to features for which categories can
-#   be encoded by the values 0 or 1.
-# * one-hot encoding: it will corresponds to features for which categories
-#   do not have a particular ordered and should be a column should be created
-#   for each category.
-# * scaling: it will corresponds to the numerical features which will be
-#   standardized.
+# * **binary encoding** will be applied to categorical columns with only too
+#   possible values (e.g. sex=male or sex=female in this example). Each binary
+#   categorical columns will be mapped to one numerical columns with 0 or 1
+#   values.
+# * **one-hot encoding** will be applied to categorical columns with more that
+#   two possible categories. This encoding will create one additional column for
+#   each possible categorical value.
+# * **numerical scaling** numerical features which will be standardized.
 
 # %%
-binary_encoding_columns = ['MARR', 'SEX', 'SOUTH', 'UNION']
-one_hot_encoding_columns = ['OCCUPATION', 'SECTOR', 'RACE']
-scaling_columns = ['AGE', 'EDUCATION', 'EXPERIENCE']
+binary_encoding_columns = ['sex']
+one_hot_encoding_columns = ['workclass', 'education', 'marital-status',
+                            'occupation', 'relationship',
+                            'race', 'native-country']
+scaling_columns = ['age', 'education-num', 'hours-per-week',
+                   'capital-gain', 'capital-loss']
 
 # %% [markdown]
+#
 # We can now create our `ColumnTransfomer` by specifying a list of triplet
-# (preprocessor name, transformer, columns). Finally, we can merge this
-# "preprocessor" in a machine learning pipeline by adding a machine learning
-# model (e.g. a linear model) after the preprocessing.
+# (preprocessor name, transformer, columns). Finally, we can define a pipeline
+# to stack this "preprocessor" with our classifier (logistic regression).
 
 # %%
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import RidgeCV
 
 preprocessor = ColumnTransformer([
     ('binary-encoder', OrdinalEncoder(), binary_encoding_columns),
@@ -339,9 +415,9 @@ preprocessor = ColumnTransformer([
      one_hot_encoding_columns),
     ('standard-scaler', StandardScaler(), scaling_columns)
 ])
-model = make_pipeline(preprocessor, RidgeCV())
-score = cross_val_score(model, data, target)
-print(f"The R2 score is: {score.mean():.2f} +- {score.std():.2f}")
+model = make_pipeline(preprocessor, LogisticRegression(max_iter=1000))
+score = cross_val_score(model, data, target, cv=5)
+print(f"The accuracy is: {score.mean():.3f} +- {score.std():.3f}")
 print(f"The different scores obtained are: \n{score}")
 
 

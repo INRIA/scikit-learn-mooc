127ed8ef63fe3ab5be6618a9bc4b52537ecc8730
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-08-29 14:49:07 +0200

    CLN remove notebooks

--- notebooks/figures/plot_helpers.py
+++
@@ -1,4 +0,0 @@
-from matplotlib.colors import ListedColormap
-
-cm3 = ListedColormap(['#1f77b4', '#ff7f0e', '#2ca02c'])
-cm2 = ListedColormap(['#1f77b4', '#ff7f0e'])
--- notebooks/solutions/13_cross_validation.py
+++
@@ -1,2 +0,0 @@
-cv = KFold(n_splits=3)
-cross_val_score(classifier, iris.data, iris.target, cv=cv)

51320d0103c4aba902d4398a58af775373276a5c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-09-01 14:22:32 +0200

    SYNC exercise notebook 3

--- python_scripts/03_basic_preprocessing_tuning_exercise_01_solution.py
+++ python_scripts/03_basic_preprocessing_tuning_exercise_01_solution.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: notebooks//ipynb,python_scripts//py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # #  Exercise 01
 # The goal is to find the best set of hyper-parameters which maximize the

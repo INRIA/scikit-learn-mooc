cc273aedf3a6a74c22779ee4ad6e4a013ee56607
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-10-04 19:33:50 +0200

    FIX ignore ipynb_checkpoints folder with Makefile (#6)

--- build_scripts/check-python-scripts.py
+++ build_scripts/check-python-scripts.py
@@ -1,8 +1,17 @@
 import sys
 import os
 
+# TODO: we could get the list from .gitignore
+IGNORE_LIST = [
+    '.ipynb_checkpoints',
+]
+
 folder = sys.argv[1]
-non_python_files = [fn for fn in os.listdir(folder) if not fn.endswith('.py')]
+non_python_files = [
+    fn
+    for fn in os.listdir(folder)
+    if fn not in IGNORE_LIST and not fn.endswith('.py')
+]
 
 if non_python_files:
     raise RuntimeError(f'Looks like you have non python files in {folder}:\n'

8deda6f84978c9eb0afba1ea0ae47a9a187a4414
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-06-10 11:49:23 +0200

    add introduction

---
+++ python_scripts/dev_metrics.py
@@ -0,0 +1,26 @@
+# %% [markdown]
+# # Evaluate of your predictive model
+
+# %% [markdown]
+# ## Introduction
+# Machine-learning models rely on optimizing an objective function, by seeking
+# its minimum or maximum. It is important to understand that this objective
+# function is usually decoupled from the evaluation metric that we want to
+# optimize in practice. The objective function serves as a proxy to the
+# evaluation metric.
+#
+# While other notebooks will give insights regarding algorithms and their
+# associated objective functions, in this notebook we will focus on the
+# metrics used to evaluate the performance of a predictive model.
+#
+# Selecting an evaluation metric will mainly depend on the model chosen to
+# solve our datascience problem.
+
+# %% [markdown]
+# ## Classification
+
+# %% [markdown]
+# ## Regression
+
+# %% [markdown]
+# ## Clustering

3277f31703c57b705d97accfc738629947e7e5a0
Gael Varoquaux <gael.varoquaux@normalesup.org>
2020-06-10 15:03:35 +0200

    preferred format

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 # To use jupytext in binder
 c.NotebookApp.contents_manager_class = 'jupytext.TextFileContentsManager'  # noqa
-c.ContentsManager.preferred_jupytext_formats_read = 'py:sphinx'  # noqa
+c.ContentsManager.preferred_jupytext_formats_read = 'py:percent'  # noqa
 c.ContentsManager.sphinx_convert_rst2md = True  # noqa

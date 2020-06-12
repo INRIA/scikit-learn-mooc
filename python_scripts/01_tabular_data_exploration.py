c4a1aff7944b4d79dc17cb5add2a378b046d9a6d
twsthomas <twsthomas@gmail.com>
2020-06-12 17:18:47 +0200

    minor suggestions

--- python_scripts/01_tabular_data_exploration.py
+++ python_scripts/01_tabular_data_exploration.py
@@ -64,13 +64,6 @@ target_column = 'class'
 adult_census[target_column].value_counts()
 
 # %% [markdown]
-# Note: classes are slightly imbalanced. Class imbalance happens often in
-# practice and may need special techniques for machine learning. For example in
-# a medical setting, if we are trying to predict whether patients will develop
-# a rare disease, there will be a lot more healthy patients than ill patients in
-# the dataset.
-
-# %% [markdown]
 # The dataset contains both numerical and categorical data. Numerical values
 # can take continuous values for example `age`. Categorical values can have a
 # finite number of values, for example `native-country`.
@@ -100,7 +93,7 @@ adult_census = adult_census[all_columns]
 #   learning
 # * you need to check that the information you need for your task is indeed present in
 # the dataset
-# * inspecting the data is a good way to find peculiarities. These can can
+# * inspecting the data is a good way to find peculiarities. These can
 #   arise during data collection (for example, malfunctioning sensor or missing
 #   values), or from the way the data is processed afterwards (for example capped
 #   values).
@@ -121,7 +114,7 @@ _ = adult_census.hist(figsize=(20, 10))
 # without looking much further. We'll do that later in this notebook.
 # * hours per week peaks at 40, this was very likely the standard number of
 # working hours at the time of the data collection
-# * most values of capital-gain and capital-loss are close to zero
+# * most values of capital-gain and capital-loss are in the first bin (close to zero)
 
 # %% [markdown]
 # For categorical variables, we can look at the distribution of values:
@@ -139,7 +132,7 @@ adult_census['education'].value_counts()
 
 # %%
 import pandas_profiling
-adult_census.profile_report()
+# adult_census.profile_report()
 
 # %% [markdown]
 # As noted above, `education-num` distribution has two clear peaks around 10
@@ -188,20 +181,20 @@ _ = sns.pairplot(data=adult_census[:n_samples_to_plot], vars=columns,
 # These hand-written rules could work reasonably well without the need for any
 # machine learning. Note however that it is not very easy to create rules for
 # the region `40 < hours-per-week < 60` and `30 < age < 70`. We can hope that
-# machine learning can help in this region. Also note that visualization can
-# help creating hand-written rules but is limited to 2 dimensions (maybe 3
-# dimensions), whereas machine learning models can build models in
+# machine learning can help in this region. If visualization can
+# help creating hand-written rules, it is limited to 2 or 3 dimensions, 
+# when machine learning models can learn rules in
 # high-dimensional spaces.
 #
 # Another thing worth mentioning in this plot: if you are young (less than 25
 # year-old roughly) or old (more than 70 year-old roughly) you tend to work
 # less. This is a non-linear relationship between age and hours
-# per week. Some machine learning models can only capture linear interactions so
+# per week. Linear machine learning models can only capture linear interactions, so
 # this may be a factor when deciding which model to chose.
 #
-# In a machine-learning setting, we will use an algorithm to automatically
+# In a machine-learning setting, algorithm automatically
 # decide what should be the "rules" in order to make predictions on new data.
-# We can check which set of simple rules a decision tree would grasp using the
+# Let's visualize which set of simple rules a decision tree would grasp using the
 # same data.
 
 
@@ -259,8 +252,8 @@ y = LabelEncoder().fit_transform(
     data_subset[target_column].to_numpy())
 
 # %% [markdown]
-# We will create a decision tree which we will keep really simple on purpose.
-# We will only allow a maximum of 2 rules to interpret the results.
+# We will create a simple decision tree with a maximum of 2 rules, in order
+# to interpret the results.
 
 # %%
 from sklearn.tree import DecisionTreeClassifier
@@ -272,8 +265,8 @@ tree = DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes,
 tree.fit(X, y)
 
 # %% [markdown]
-# We can now check the set of rules learnt by the tree then visually check
-# what theses rules look like.
+# `plot_tree` will allow us to visually check the set of rules learnt by 
+# the decision tree.
 
 # %%
 _ = plot_tree(tree)
@@ -283,11 +276,11 @@ _ = plot_tree(tree)
 plot_tree_decision_function(tree, X, y)
 
 # %% [markdown]
-# Allowing only 3 leaves in the tree, we get similar rules to the ones we
+# By allowing only 3 leaves in the tree, we get similar rules that the ones we
 # designed by hand:
-# * the persons younger than 28.5 year-old will be considered in the class
+# * the persons younger than 28.5 year-old (X[0] < 28.5) will be considered in the class
 #   earning `<= 50K`.
-# * the persons older than 28.5 and working more than 40.5 hours-per-week
+# * the persons older than 28.5 and working more than 40.5 hours-per-week (X[1] > 40.5)
 #   will be considered in the class earning `> 50K`, while the persons working
 #   below 40.5 hours-per-week, will be considered in the class
 #   earning `<= 50K`.
@@ -296,13 +289,18 @@ plot_tree_decision_function(tree, X, y)
 #
 # In this notebook we have:
 # * loaded the data from a CSV file using `pandas`
-# * looked at the kind of variables in the dataset, and differentiated
+# * looked the differents kind of variables to differentiate
 #   between categorical and numerical variables
 # * inspected the data with `pandas`, `seaborn` and `pandas_profiling`. Data inspection
 #   can allow you to decide whether using machine learning is appropriate for
 #   your data and to highlight potential peculiarities in your data
 #
 # Key ideas discussed:
+# * A first glance to the data can give some insights of features importance
+# * It is possible to visualize the decision rules from a decision tree
+# 
+# 
+# Ideas not discussed in this notebook:
 # * if your target variable is imbalanced (e.g., you have more samples from one
 #   target category than another), you may need special techniques for machine
 #   learning

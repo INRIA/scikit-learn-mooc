7d82551ce220d7d3de35af8f925772506d68f6f6
Loïc Estève <loic.esteve@ymail.com>
2020-06-17 11:47:51 +0200

    Improve Makefile to make sure rendered notebook is not empty (#17)

---
+++ build_scripts/convert-python-script-to-notebook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+PYTHON_SCRIPT=$1
+NOTEBOOK=$2
+JUPYTER_KERNEL=$3
+
+TMP_NOTEBOOK=${NOTEBOOK/.ipynb/_tmp.ipynb}
+jupytext --to notebook --output $TMP_NOTEBOOK $PYTHON_SCRIPT
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=None \
+        --ExecutePreprocessor.kernel_name=$JUPYTER_KERNEL --inplace $TMP_NOTEBOOK
+
+# Only move the temporary notebook if all the previous command have been successful
+mv $TMP_NOTEBOOK $NOTEBOOK

758024eb7ff39648eba05aaf9927561ece41fee1
Loïc Estève <loic.esteve@ymail.com>
2020-06-19 10:18:35 +0200

    Switch to CircleCI

---
+++ build_tools/circle/run_notebooks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xe
+
+apt-get install make
+
+source /opt/conda/etc/profile.d/conda.sh
+conda update --yes conda
+conda create -n testenv --yes pip python=3.7
+conda activate testenv
+pip install -r requirements.txt
+pip install jupytext yapf
+
+make --always-make

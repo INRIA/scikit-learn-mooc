0a82c81de791deb8db257fb3984792e71efaedf3
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-07-24 19:16:36 +0200

    finish with bagging and random forest

--- python_scripts/ensemble.py
+++ python_scripts/ensemble.py
@@ -156,6 +156,13 @@ print(f"R2 score: {bagging_regressor.score(X_test, y_test):.3f}")
 # boosting.
 #
 # ## Bagging
+#
+# We will first focus on the bagging algorithm. Bagging stands for bootstrap
+# aggregating. Indeed, it uses bootstrap samples to learn several learners.
+# At predict time, the predictions of each learner are aggregated to give the
+# final predictions.
+#
+# Let's define a simple dataset that we already used in some previous notebook.
 
 # %%
 import numpy as np
@@ -163,14 +170,51 @@ import matplotlib.pyplot as plt
 
 rng = np.random.RandomState(0)
 
-n_sample = 50
-x_max, x_min = 1.4, -1.4
-len_x = (x_max - x_min)
-x = rng.rand(n_sample) * len_x - len_x/2
-noise = rng.randn(n_sample) * .3
-y = x ** 3 - 0.5 * x ** 2 + noise
+
+def generate_data(n_samples=50):
+    x_max, x_min = 1.4, -1.4
+    len_x = x_max - x_min
+    x = rng.rand(n_samples) * len_x - len_x / 2
+    noise = rng.randn(n_samples) * 0.3
+    y = x ** 3 - 0.5 * x ** 2 + noise
+    return x, y
+
+
+x, y = generate_data(n_samples=50)
 
 plt.scatter(x, y,  color='k', s=9)
+plt.xlabel("Feature")
+_ = plt.ylabel("Target")
+
+# %% [markdown]
+# The link between our feature and the target to predict is non-linear.
+# However, a decision tree is capable to fit such data
+
+# %%
+tree = DecisionTreeRegressor(max_depth=3, random_state=0)
+tree.fit(x.reshape(-1, 1), y)
+
+grid = np.linspace(np.min(x), np.max(x), num=30)
+y_pred = tree.predict(grid.reshape(-1, 1))
+
+plt.scatter(x, y, color="k", s=9)
+plt.plot(grid, y_pred, label="Tree fitting")
+plt.xlabel("Feature")
+plt.ylabel("Target")
+plt.legend()
+
+# %% [markdown]
+# Let's see how we can use bootstraping to learn several trees.
+#
+# ### Bootstrap sample
+#
+# Let's first define what is a bootstrap sample: it is a resampling with
+# replacement of the same size than the original sample. Thus, the bootstrap
+# sample will contain some of the data points several time and some of the
+# original data points will not be present in the sample.
+#
+# We will create a function that given `x` and `y` will return a bootstrap
+# sample `x_bootstrap` and `y_bootstrap`.
 
 
 # %%
@@ -183,9 +227,16 @@ def bootstrap_sample(x, y):
     return x_bootstrap_sample, y_bootstrap_sample
 
 
+# %% [markdown]
+# Let's generate 3 bootstrap sample and check the difference with the original
+# dataset.
+
+
 # %%
 n_bootstrap = 3
-_, axs = plt.subplots(ncols=n_bootstrap, figsize=(16, 6))
+_, axs = plt.subplots(
+    ncols=n_bootstrap, figsize=(16, 6), sharex=True, sharey=True
+)
 
 for idx, (ax, _) in enumerate(zip(axs, range(n_bootstrap))):
     x_bootstrap_sample, y_bootstrap_sample = bootstrap_sample(x, y)
@@ -193,9 +244,38 @@ for idx, (ax, _) in enumerate(zip(axs, range(n_bootstrap))):
         x_bootstrap_sample, y_bootstrap_sample,
     )
     ax.set_title(f"Bootstrap sample #{idx}")
+    ax.set_ylabel("Target")
+    ax.set_xlabel("Feature")
+
+# %% [markdown]
+# We can observe that the 3 generated bootstrap are all different. We can even
+# check the amount of samples in the bootstrap sample which are present in the
+# original dataset.
 
 # %%
-_, axs = plt.subplots(ncols=n_bootstrap, figsize=(16, 6))
+# we need to generate a larger set to have a good estimate
+x_huge, y_huge = generate_data(n_samples=10000)
+x_bootstrap_sample, y_bootstrap_sample = bootstrap_sample(x_huge, y_huge)
+
+print(
+    f"Percentage of samples present in the original dataset: "
+    f"{np.unique(x_bootstrap_sample).size / x_bootstrap_sample.size * 100:.1f}"
+    f"%"
+)
+
+# %% [markdown]
+# Theoretically, 63.2% of the original data points of the original dataset will
+# be present in the bootstrap sample. The other 36.8% are just some repeated
+# samples.
+#
+# So, we are able to generate as many datasets, all slightly different. Now,
+# we can fit a decision tree for each of these datasets and they will be all
+# slightly different as well.
+
+# %%
+_, axs = plt.subplots(
+    ncols=n_bootstrap, figsize=(16, 6), sharex=True, sharey=True,
+)
 
 forest = []
 for idx, (ax, _) in enumerate(zip(axs, range(n_bootstrap))):
@@ -210,69 +290,122 @@ for idx, (ax, _) in enumerate(zip(axs, range(n_bootstrap))):
 
     grid = np.linspace(np.min(x), np.max(x), num=300)
     y_pred = forest[-1].predict(grid.reshape(-1, 1))
-    ax.plot(grid, y_pred, linewidth=3)
+    ax.plot(grid, y_pred, linewidth=3, label="Fitted tree")
 
-    ax.set_title(f"Fitted tree on boostrap sample #{idx}")
+    ax.legend()
+    ax.set_ylabel("Target")
+    ax.set_xlabel("Features")
+    ax.set_title(f"Bootstrap #{idx}")
+
+# %% [markdown]
+# We can plot all trees decision function on the same plot to highlight that
+# they are indeed different.
 
 # %%
-
 _, ax = plt.subplots()
-ax.scatter(x, y)
+ax.scatter(x, y, color="k", alpha=0.4)
 y_pred_forest = []
 for tree_idx, tree in enumerate(forest):
     y_pred = tree.predict(grid.reshape(-1, 1))
     ax.plot(
-        grid, y_pred, "-.", label=f"Tree #{tree_idx} predictions", linewidth=3,
+        grid,
+        y_pred,
+        "--",
+        label=f"Tree #{tree_idx} predictions",
+        linewidth=3,
+        alpha=0.8,
     )
     y_pred_forest.append(y_pred)
 
-y_pred_forest = np.mean(y_pred_forest, axis=0)
-ax.plot(
-    grid, y_pred_forest, "--", label="Bagging predictions", linewidth=3,
-)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+_ = plt.legend()
 
-plt.legend()
-
-# %%
-_, axs = plt.subplots(ncols=n_bootstrap, figsize=(16, 6))
-
-random_forest = []
-for idx, (ax, _) in enumerate(zip(axs, range(n_bootstrap))):
-    x_bootstrap_sample, y_bootstrap_sample = bootstrap_sample(x, y)
-    ax.scatter(x_bootstrap_sample, y_bootstrap_sample)
-
-    random_forest.append(
-        DecisionTreeRegressor(
-            max_depth=3, max_features="sqrt", random_state=0
-            ).fit(
-            x_bootstrap_sample.reshape(-1, 1), y_bootstrap_sample
-        )
-    )
-
-    grid = np.linspace(np.min(x), np.max(x), num=300)
-    y_pred = random_forest[-1].predict(grid.reshape(-1, 1))
-    ax.plot(grid, y_pred, linewidth=3)
-
-    ax.set_title(f"Fitted tree on boostrap sample #{idx}")
+# %% [markdown]
+# ### Aggregating
+#
+# Once that our trees are fitted and we are able to get predictions for each of
+# them, we also need to combine these predictions. The most straightforward
+# approach is to average the different predictions. We can plot the average
+# predictions on the plot that we shown previously.
 
 # %%
-
 _, ax = plt.subplots()
-ax.scatter(x, y)
+ax.scatter(x, y, color="k", alpha=0.4)
 y_pred_forest = []
-for tree_idx, tree in enumerate(random_forest):
+for tree_idx, tree in enumerate(forest):
     y_pred = tree.predict(grid.reshape(-1, 1))
     ax.plot(
-        grid, y_pred, "-.", label=f"Tree #{tree_idx} predictions", linewidth=3,
+        grid,
+        y_pred,
+        "--",
+        label=f"Tree #{tree_idx} predictions",
+        linewidth=3,
+        alpha=0.5,
     )
     y_pred_forest.append(y_pred)
 
+# Averaging the predictions
 y_pred_forest = np.mean(y_pred_forest, axis=0)
 ax.plot(
-    grid, y_pred_forest, "--", label="Random forest predictions", linewidth=3,
+    grid,
+    y_pred_forest,
+    "-",
+    label="Averaged predictions",
+    linewidth=3,
+    alpha=0.8,
 )
 
-plt.legend()
+plt.xlabel("Feature")
+plt.ylabel("Target")
+_ = plt.legend()
 
+# %% [markdown]
+# The red line on the plot is showing what would be the final predictions of
+# our model.
+#
+# ## Random Forest
+#
+# One of the widely used algorithm in machine learning is Random Forest.
+# Random Forest is a modification of the bagging algorithm. In bagging, any
+# classifier or regressor can be used. Random forest limit this base classifier
+# or regressor to be a decision tree, similarly to what we shown in the
+# previous section. There is a noticeable difference in classification: when
+# searching for the best split, only a subset of the original feature will be
+# used. This subset will be equal to the squared root of the original number
+# of feature. In regression, the total number of available feature will be
+# used.
+#
+# We will illustrate the usage of a random forest and compare it with the
+# bagging regressor on the "California housing" dataset.
 
 # %%
+X, y = california_housing.data, california_housing.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+from sklearn.ensemble import RandomForestRegressor
+
+random_forest = RandomForestRegressor(
+    n_estimators=100, random_state=0, n_jobs=-1
+)
+bagging = BaggingRegressor(
+    base_estimator=DecisionTreeRegressor(random_state=0),
+    n_estimators=100,
+    n_jobs=-1,
+)
+
+random_forest.fit(X_train, y_train)
+bagging.fit(X_train, y_train)
+
+print(
+    f"Performance of random forest: {random_forest.score(X_test, y_test):.3f}"
+)
+print(f"Performance of bagging: {bagging.score(X_test, y_test):.3f}")
+
+# %% [markdown]
+# First, we can observe that we cannot give the `base_estimator` to the
+# random forest regressor since it is already fixed. Finally, we can see that
+# the score on the testing set is exactly the same. In classification setting,
+# we would need to pass a tree instance with the parameter
+# `max_features="sqrt"` if we want the bagging classifier and random forest
+# classifier to behave the same.

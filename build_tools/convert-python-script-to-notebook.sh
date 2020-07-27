f4ef609c80c2c6e9ac59c1f57c7b3380f6f238fd
Loïc Estève <loic.esteve@ymail.com>
2020-07-27 14:11:30 +0200

    Fix jupyter nbconvert option.

--- build_tools/convert-python-script-to-notebook.sh
+++ build_tools/convert-python-script-to-notebook.sh
@@ -8,7 +8,7 @@ JUPYTER_KERNEL=$3
 TMP_NOTEBOOK=${NOTEBOOK/.ipynb/_tmp.ipynb}
 jupytext --to notebook --output $TMP_NOTEBOOK $PYTHON_SCRIPT
 jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=300 \
-        --executepreprocessor.kernel_name=$JUPYTER_KERNEL --inplace $TMP_NOTEBOOK
+        --ExecutePreprocessor.kernel_name=$JUPYTER_KERNEL --inplace $TMP_NOTEBOOK
 
 # Only move the temporary notebook if all the previous command have been successful
 mv $TMP_NOTEBOOK $NOTEBOOK

56279bce6f291370ac51d17b97e1fc24ede95fa2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-10-24 11:37:33 +0200

    iter

--- python_scripts/cross_validation.py
+++ python_scripts/cross_validation.py
@@ -745,3 +745,189 @@ print(f"The average accuracy is {test_score.mean():.3f}")
 # We see that this strategy is the less optimistic regarding the model
 # performance. However, this is the most reliable if our goal is to make
 # handwritten digits recognition writers independent.
+#
+# ### Non i.i.d. data
+# In machine-learning, it is quite common to assume that the data are i.i.d
+# meaning that the generative process do not have any memory of past samples
+# to generate new samples.
+#
+# This assumption is usually violated when dealing with time series. Samples
+# from the future will be generated using samples from the past.
+#
+# We will take an example to highlight the issue of such non-i.i.d. data
+# in the previous cross-validation strategies presented. We are going to load
+# financial quotations from some energy companies.
+
+# %%
+symbols = {
+    "TOT": "Total",
+    "XOM": "Exxon",
+    "CVX": "Chevron",
+    "COP": "ConocoPhillips",
+    "VLO": "Valero Energy",
+}
+url = (
+    "https://raw.githubusercontent.com/scikit-learn/examples-data/"
+    "master/financial-data/{}.csv"
+)
+
+quotes = {}
+for symbol in symbols:
+    data = pd.read_csv(url.format(symbol), index_col=0, parse_dates=True)
+    quotes[symbols[symbol]] = data["open"]
+quotes = pd.DataFrame(quotes)
+
+# %% [markdown]
+# We can start by plotting the different financial quotations.
+
+# %%
+_, ax = plt.subplots(figsize=(10, 6))
+_ = quotes.plot(ax=ax)
+
+# %% [markdown]
+# We can formulate the following regression problem. We would like to be
+# able to predict the quotation of Chevron using all other energie companies'
+# quotations.
+
+# %%
+X, y = quotes.drop(columns=["Chevron"]), quotes["Chevron"]
+
+# %% [markdown]
+# We will use a decision tree regressor which we expect to overfit and thus not
+# being able to generalize to unseen data. We will use a `ShuffleSplit`
+# cross-validation to check the performance of our model.
+
+# %%
+from sklearn.tree import DecisionTreeRegressor
+
+regressor = DecisionTreeRegressor()
+
+cv = ShuffleSplit(random_state=0)
+results = cross_validate(regressor, X_train, y_train, cv=cv, n_jobs=-1)
+test_score = results["test_score"]
+print(f"The mean R2 is: {test_score.mean():.2f}")
+
+# %% [markdown]
+# Surprisingly, we get outstanding performance. We will try to investigate
+# and find the reason of such good results with a model that is expected to
+# fail. We previously mentioned that `ShuffleSplit` is an iterative
+# cross-validation scheme which shuffle data and split. We will simplify
+# this procedure with a single split and plot the prediction. We can use
+# `train_test_split` for this purpose.
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, shuffle=True, random_state=0,
+)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+# Affect the index of `y_test` to ease the plotting
+y_pred = pd.Series(y_pred, index=y_test.index)
+
+# %% [markdown]
+# Let's check the performance of our model on this split.
+
+# %%
+from sklearn.metrics import r2_score
+
+test_score = r2_score(y_test, y_pred)
+print(f"The R2 on this single split is: {test_score:.2f}")
+
+# %% [markdown]
+# We obtain similar good results in terms of R2. We will plot the training,
+# testing and predictions samples.
+
+# %%
+_, ax = plt.subplots(figsize=(10, 8))
+y_train.plot(ax=ax, label="Training")
+y_test.plot(ax=ax, label="Testing")
+y_pred.plot(ax=ax, label="Prediction")
+_ = plt.legend()
+
+# %% [markdown]
+# So in this context, it seems that the model predictions are following the
+# testing. But we can as well see that the testing samples are next to some
+# training sample. And with these time-series, we see that there is a
+# relationship between a sample at time `t` and a sample at `t+1`. In this
+# case, we are violating the i.i.d. assumption. The insight to get is the
+# following: a model can output of its training set at time `t` for a testing
+# sample at time `t+1`. This prediction will be closed to the true value even
+# if our model did not learn anything else than memorizing the training
+# dataset.
+#
+# An easy way to verify this hypothesis is to not shuffle the data when making
+# the split. In this case, we will use the first 75% of the data to train and
+# the remaining data to test.
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, shuffle=False, random_state=0,
+)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+y_pred = pd.Series(y_pred, index=y_test.index)
+
+# %%
+test_score = r2_score(y_test, y_pred)
+print(f"The R2 on this single split is: {test_score:.2f}")
+
+# %% [markdown]
+# In this case, we see that our model is not magical anymore. Indeed, it
+# performs worse than just predicting the mean of the target. We can check
+# visually what we are predicting.
+
+# %%
+_, ax = plt.subplots(figsize=(10, 8))
+y_train.plot(ax=ax, label="Training")
+y_test.plot(ax=ax, label="Testing")
+y_pred.plot(ax=ax, label="Prediction")
+_ = plt.legend()
+
+# %% [markdown]
+# Now, we see that our model cannot predict anything because it doesn't have
+# information of samples around the testing sample. Let's check how we could
+# have make a proper cross-validation scheme to get a good performance
+# estimate.
+#
+# One solution would be to group the samples into time blocks, e.g. by quarter,
+# and predict for each of these group by using information from the other
+# groups. We can use the `LeaveOneGroupOut` cross-validation for this purpose.
+
+# %%
+from sklearn.model_selection import LeaveOneGroupOut
+
+groups = quotes.index.to_period("Q")
+cv = LeaveOneGroupOut()
+results = cross_validate(
+    regressor, X, y, cv=cv, groups=groups,
+    n_jobs=-1
+)
+test_score = results["test_score"]
+print(f"The mean R2 is: {test_score.mean():.2f}")
+
+# %% [markdown]
+# In this case, we see that we are not able to make good prediction which is
+# less surprising than our original results.
+#
+# Another thing to consider is the actual application of our solution. If our
+# model is developed for forecasting (i.e. predict future data from past data),
+# we should not use training data which with are ulterior to the testing data.
+# In this case, we can use the `TimeSeriesSplit` cross-validation to enforce
+# this behaviour.
+
+# %%
+from sklearn.model_selection import TimeSeriesSplit
+
+cv = TimeSeriesSplit(n_splits=groups.nunique())
+results = cross_validate(
+    regressor, X, y, cv=cv, groups=groups,
+    n_jobs=-1
+)
+test_score = results["test_score"]
+print(f"The mean R2 is: {test_score.mean():.2f}")
+
+# %% [markdown]
+# ## Nested cross-validation
+
+# %% [markdown]
+# ## When cross-validation is not enough

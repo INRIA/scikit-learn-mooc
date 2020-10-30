43876a2fe33e97078dcee96e26b2be8c45b9d732
Loïc Estève <loic.esteve@ymail.com>
2020-10-30 17:44:10 +0100

    wip

---
+++ python_scripts/02_numerical_pipeline_scaling.py
@@ -0,0 +1,196 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# TODO do an intro here even for shorter notebooks?
+# TODO better title
+# TODO do I want to remind people what we did before i.e. 
+# # Preprocessing for numerical features
+
+# %% [markdown]
+# Let's first load the data as we did in the previous notebook.
+
+# %%
+import pandas as pd
+
+df = pd.read_csv("../datasets/adult-census.csv")
+
+target_name = "class"
+target = df[target_name]
+
+data = df.drop(columns=[target_name, "fnlwgt"])
+
+# %% [markdown]
+# We only work keep numerical features
+
+# %%
+from sklearn.compose import make_column_selector as selector
+
+numerical_columns_selector = selector(dtype_include=["int", "float"])
+numerical_columns = numerical_columns_selector(data)
+numerical_columns
+
+data_numeric = data[numerical_columns]
+
+# %% [markdown]
+# We do a train-test split for evaluation
+
+# %%
+from sklearn.model_selection import train_test_split
+
+data_train, data_test, target_train, target_test = train_test_split(
+    data_numeric, target, random_state=42)
+
+
+# %% [markdown]
+# Let's consider the `ConvergenceWarning` message that was raised previously
+# when calling the `fit` method to train our model. This warning informs us
+# that our model stopped learning because it reached the maximum number of
+# iterations allowed by the user. This could potentially be detrimental for the
+# model accuracy. We can follow the (bad) advice given in the warning message
+# and increase the maximum number of iterations allowed.
+
+# %%
+from sklearn.linear_model import LogisticRegression
+import time
+
+model = LogisticRegression(max_iter=50000)
+start = time.time()
+model.fit(data_train, target_train)
+elapsed_time = time.time() - start
+
+# %%
+print(
+    f"The accuracy using a {model.__class__.__name__} is "
+    f"{model.score(data_test, target_test):.3f} with a fitting time of "
+    f"{elapsed_time:.3f} seconds in {model.n_iter_} iterations")
+
+# %% [markdown]
+# We now observe a longer training time but no significant improvement in
+# the predictive performance. Instead of increasing the number of iterations, we
+# can try to help fit the model faster by scaling the data first. A range of
+# preprocessing algorithms in scikit-learn allows us to transform the input data
+# before training a model.
+#
+# In our case, we will standardize the data and then train a new logistic
+# regression model on that new version of the dataset.
+
+# %%
+data_train.describe()
+
+# %%
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+data_train_scaled = scaler.fit_transform(data_train)
+data_train_scaled
+
+# %%
+data_train_scaled = pd.DataFrame(data_train_scaled,
+                                 columns=data_train.columns)
+data_train_scaled.describe()
+
+# %% [markdown]
+# We can easily combine these sequential operations
+# with a scikit-learn `Pipeline`, which chains together operations and can be
+# used like any other classifier or regressor. The helper function
+# `make_pipeline` will create a `Pipeline` by giving as arguments the successive
+# transformations to perform followed by the classifier or regressor model.
+
+# %%
+from sklearn.pipeline import make_pipeline
+
+model = make_pipeline(StandardScaler(),
+                      LogisticRegression())
+start = time.time()
+model.fit(data_train, target_train)
+elapsed_time = time.time() - start
+
+# %%
+print(
+    f"The accuracy using a {model.__class__.__name__} is "
+    f"{model.score(data_test, target_test):.3f} with a fitting time of "
+    f"{elapsed_time:.3f} seconds in {model[-1].n_iter_} iterations")
+
+# %% [markdown]
+# We can see that the training time and the number of iterations is much shorter
+# while the predictive performance (accuracy) stays the same.
+#
+# In the previous example, we split the original data into a training set and a
+# testing set. This strategy has several issues: in the setting where the amount
+# of data is limited, the subset of data used to train or test will be small;
+# and the splitting was done in a random manner and we have no information
+# regarding the confidence of the results obtained.
+#
+# Instead, we can use cross-validation. Cross-validation consists of
+# repeating this random splitting into training and testing sets and aggregating
+# the model performance. By repeating the experiment, one can get an estimate of
+# the variability of the model performance.
+#
+# The function `cross_val_score` allows for such experimental protocol by giving
+# the model, the data and the target. Since there exists several
+# cross-validation strategies, `cross_val_score` takes a parameter `cv` which
+# defines the splitting strategy.
+
+
+# %%
+# %%time
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(model, data_numeric, target, cv=5)
+
+# %%
+scores
+
+# %%
+print(f"The mean cross-validation accuracy is: "
+      f"{scores.mean():.3f} +/- {scores.std():.3f}")
+
+# %% [markdown]
+# Note that by computing the standard-deviation of the cross-validation scores
+# we can get an idea of the uncertainty of our estimation of the predictive
+# performance of the model: in the above results, only the first 2 decimals seem
+# to be trustworthy. Using a single train / test split would not allow us to
+# know anything about the level of uncertainty of the accuracy of the model.
+#
+# Setting `cv=5` created 5 distinct splits to get 5 variations for the training
+# and testing sets. Each training set is used to fit one model which is then
+# scored on the matching test set. This strategy is called K-fold
+# cross-validation where `K` corresponds to the number of splits.
+#
+# The figure helps visualize how the dataset is partitioned
+# into train and test samples at each iteration of the cross-validation
+# procedure:
+#
+# ![Cross-validation diagram](../figures/cross_validation_diagram.png)
+#
+# For each cross-validation split, the procedure trains a model on the
+# concatenation of the red samples and evaluate the score of the model
+# by using the blue samples. Cross-validation is therefore computationally
+# intensive because it requires training several models instead of one.
+#
+# Note that the `cross_val_score` method above discards the 5 models that
+# were trained on the different overlapping subset of the dataset.
+# The goal of cross-validation is not to train a model, but rather to
+# estimate approximately the generalization performance of a model that
+# would have been trained to the full training set, along with an estimate
+# of the variability (uncertainty on the generalization accuracy).
+
+# %% [markdown]
+# In this notebook we have:
+# * **split** our dataset into a training dataset and a testing dataset
+# * fitted a **logistic regression** model
+# * seen the importance of **scaling numerical variables**
+# * used a **pipeline** to fit both the scaler and the logistic regression
+# * assessed the performance of our model via **cross-validation**
---
+++ python_scripts/03_categorical_pipeline_column_transformer.py
@@ -0,0 +1,206 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+# %%
+
+# TODO intro
+# %% [markdown]
+# # Using numerical and categorical variables together
+#
+# Let us reload the dataset
+
+# %%
+import pandas as pd
+
+df = pd.read_csv("../datasets/adult-census.csv")
+
+target_name = "class"
+target = df[target_name]
+
+data = df.drop(columns=[target_name, "fnlwgt"])
+
+# %% [markdown]
+# We separate categorical and numerical variables as we did previously
+
+# %%
+from sklearn.compose import make_column_selector as selector
+
+categorical_columns_selector = selector(dtype_exclude=["int", "float"])
+categorical_columns = categorical_columns_selector(data)
+
+numerical_columns_selector = selector(dtype_include=["int", "float"])
+numerical_columns = numerical_columns_selector(data)
+
+# %% [markdown]
+# ## ColumnTransformer
+#
+# In the previous sections, we saw that we need to treat data differently
+# depending on their nature (i.e. numerical or categorical).
+#
+# Scikit-learn provides a `ColumnTransformer` class which will send
+# specific columns to a specific transformer, making it easy to fit a single
+# predictive model on a dataset that combines both kinds of variables together
+# (heterogeneously typed tabular data).
+#
+# We can first define the columns depending on their data type:
+# * **binary encoding** will be applied to categorical columns with only too
+#   possible values (e.g. sex=male or sex=female in this example). Each binary
+#   categorical columns will be mapped to one numerical columns with 0 or 1
+#   values.
+# * **one-hot encoding** will be applied to categorical columns with more that
+#   two possible categories. This encoding will create one additional column for
+#   each possible categorical value.
+# * **numerical scaling** numerical features which will be standardized.
+
+# %%
+binary_encoding_columns = ['sex']
+one_hot_encoding_columns = [
+    c for c in categorical_columns if c not in binary_encoding_columns]
+
+# %% [markdown]
+# We can now create our `ColumnTransfomer` by specifying a list of triplet
+# (preprocessor name, transformer, columns). Finally, we can define a pipeline
+# to stack this "preprocessor" with our classifier (logistic regression).
+
+# %%
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+preprocessor = ColumnTransformer([
+    ('binary-encoder', OrdinalEncoder(), binary_encoding_columns),
+    ('one-hot-encoder', OneHotEncoder(handle_unknown='ignore'),
+     one_hot_encoding_columns),
+    ('standard-scaler', StandardScaler(), numerical_columns)])
+
+# %%
+model = make_pipeline(
+    preprocessor, LogisticRegression(max_iter=1000))
+
+# %% [markdown]
+# Starting from `scikit-learn` 0.23, the notebooks can display an interactive
+# view of the pipelines.
+
+# %%
+from sklearn import config_context
+with config_context(display='diagram'):
+    model
+
+# %% [markdown]
+# The final model is more complex than the previous models but still follows the
+# same API:
+# - the `fit` method is called to preprocess the data then train the classifier;
+# - the `predict` method can make predictions on new data;
+# - the `score` method is used to predict on the test data and compare the
+#   predictions to the expected test labels to compute the accuracy.
+
+# %%
+from sklearn.model_selection import train_test_split
+
+data_train, data_test, target_train, target_test = train_test_split(
+    data, target, random_state=42)
+
+# %%
+_ = model.fit(data_train, target_train)
+
+# %%
+data_test.head()
+
+# %%
+model.predict(data_test)[:5]
+
+# %%
+target_test[:5]
+
+# %%
+model.score(data_test, target_test)
+
+# %% [markdown]
+# This model can also be cross-validated as usual (instead of using a single
+# train-test split):
+
+# %%
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(model, data, target, cv=5)
+scores
+
+# %%
+print(f"The accuracy is: {scores.mean():.3f} +- {scores.std():.3f}")
+
+# %% [markdown]
+# The compound model has a higher predictive accuracy than the
+# two models that used numerical and categorical variables in
+# isolation.
+
+# %% [markdown]
+# ## Fitting a more powerful model
+#
+# **Linear models** are very nice because they are usually very cheap to train,
+# **small** to deploy, **fast** to predict and give a **good baseline**.
+#
+# However, it is often useful to check whether more complex models such as an
+# ensemble of decision trees can lead to higher predictive performance.
+#
+# In the following cell we try a scalable implementation of the **Gradient Boosting
+# Machine** algorithm. For this class of models, we know that contrary to linear
+# models, it is **useless to scale the numerical features** and furthermore it is
+# both safe and significantly more computationally efficient to use an arbitrary
+# **integer encoding for the categorical variables** even if the ordering is
+# arbitrary. Therefore we adapt the preprocessing pipeline as follows:
+
+# %%
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier
+
+# For each categorical column, extract the list of all possible categories
+# in some arbritrary order.
+categories = [
+    data[column].unique()
+    for column in categorical_columns
+]
+
+preprocessor = ColumnTransformer([
+    ('categorical', OrdinalEncoder(categories=categories),
+     categorical_columns)], remainder="passthrough")
+
+model = make_pipeline(preprocessor, HistGradientBoostingClassifier())
+
+# %%
+# %%time
+_ = model.fit(data_train, target_train)
+
+# %%
+model.score(data_test, target_test)
+
+# %% [markdown]
+# We can observe that we get significantly higher accuracies with the Gradient
+# Boosting model. This is often what we observe whenever the dataset has a large
+# number of samples and limited number of informative features (e.g. less than
+# 1000) with a mix of numerical and categorical variables.
+#
+# This explains why Gradient Boosted Machines are very popular among datascience
+# practitioners who work with tabular data.
+
+# %% [markdown]
+#
+# In this notebook we have:
+# * encoded categorical features with both an **ordinal encoding** and
+#   an **one hot encoding**
+# * used a pipeline to process **both numerical and categorical** features before
+#  fitting a logistic regression
+# * seen that **gradient boosting methods** can outperforms the basic linear approach
+#

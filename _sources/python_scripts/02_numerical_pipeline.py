01984b1460a364a03efb3a718a1bdfb08fcefc1e
lesteve <lesteve@users.noreply.github.com>
2020-11-17 10:28:16 +0000

    deploy: e9afada75727c988fe81b643cbd74e98d2cca4b7

--- _sources/python_scripts/02_numerical_pipeline.py
+++ _sources/python_scripts/02_numerical_pipeline.py
@@ -75,7 +75,7 @@ data.dtypes
 # %%
 from sklearn.compose import make_column_selector as selector
 
-numerical_columns_selector = selector(dtype_include=["int", "float"])
+numerical_columns_selector = selector(dtype_exclude=object)
 numerical_columns = numerical_columns_selector(data)
 numerical_columns
 

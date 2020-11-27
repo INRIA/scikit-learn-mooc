652d13864280598469eed6190b8b54ef1a66fb86
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-11-27 10:54:08 +0100

    clean up

--- python_scripts/cross_validation.py
+++ python_scripts/cross_validation.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.6.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Train and test datasets
 # Before discussing the cross-validation framework, we will linger on the

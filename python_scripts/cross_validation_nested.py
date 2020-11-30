d089fe407d4093dfdfaa6c6a39104a383893b35c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-11-30 17:01:51 +0100

    iter

--- python_scripts/cross_validation_nested.py
+++ python_scripts/cross_validation_nested.py
@@ -1,5 +1,10 @@
 # %% [markdown]
 # # Nested cross-validation
+#
+# In this notebook, we show a pattern called **nested cross-validation** which
+# is should be used when you want to both evaluate a model and tune the
+# hyperparameters' model.
+#
 # Cross-validation is a powerful tool to evaluate the performance of a model.
 # It is also used to select the best model from a pool of models. This pool of
 # models can be the same family of predictor but with different parameters. In
@@ -9,28 +14,30 @@
 # We could also imagine that we would like to choose among heterogeneous models
 # that will similarly use the cross-validation.
 #
-# In the example below, we show a minimal example of using the utility
-# `GridSearchCV` to find the best parameters via cross-validation.
+# Before to go into details regarding the nested cross-validation, we will
+# first recall the patter used to fine tune hyperparameters' model.
+#
+# First, we will load the breast cancer dataset.
 
 # %%
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import GridSearchCV, KFold
-from sklearn.svm import SVC
 
 X, y = load_breast_cancer(return_X_y=True)
 
-param_grid = {
-    "C": [0.1, 1, 10],
-    "gamma": [.01, .1],
-}
+# %% [markdown]
+# Now, we show a minimal example of using the utility `GridSearchCV` to find
+# the best parameters via cross-validation.
 
-model = GridSearchCV(
-    estimator=SVC(),
-    param_grid=param_grid,
-    cv=KFold(),
-    n_jobs=-1,
-)
-model.fit(X, y)
+# %%
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
+param_grid = {"C": [0.1, 1, 10], "gamma": [.01, .1]}
+model_to_tune = SVC()
+
+search = GridSearchCV(
+    estimator=model_to_tune, param_grid=param_grid, n_jobs=-1)
+search.fit(X, y)
 
 # %% [markdown]
 # We recall that `GridSearchCV` will train a model with some specific parameter
@@ -42,13 +49,13 @@ model.fit(X, y)
 # maximize the mean score on the internal test sets.
 
 # %%
-print(f"The best parameter found are: {model.best_params_}")
+print(f"The best parameter found are: {search.best_params_}")
 
 # %% [markdown]
 # We can now show the mean score obtained using the parameter `best_score_`.
 
 # %%
-print(f"The mean score in CV is: {model.best_score_:.3f}")
+print(f"The mean score in CV is: {search.best_score_:.3f}")
 
 # %% [markdown]
 # At this stage, one should be extremely careful using this score. The
@@ -75,27 +82,20 @@ print(f"The mean score in CV is: {model.best_score_:.3f}")
 # the generalization score.
 
 # %%
+from sklearn.model_selection import cross_val_score, KFold
+
 # Declare the inner and outer cross-validation
 inner_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 
 # Inner cross-validation for parameter search
 model = GridSearchCV(
-    estimator=SVC(), param_grid=param_grid, cv=inner_cv,
-    n_jobs=-1,
-)
+    estimator=model_to_tune, param_grid=param_grid, cv=inner_cv, n_jobs=-1)
 
 # Outer cross-validation to compute the generalization score
-from sklearn.model_selection import cross_validate
-
-result = cross_validate(
-    model, X, y, cv=outer_cv, n_jobs=-1,
-)
-test_score = result["test_score"].mean()
-print(
-    f"The mean score using nested cross-validation is: "
-    f"{test_score.mean():.3f}"
-)
+test_score = cross_val_score(model, X, y, cv=outer_cv, n_jobs=-1)
+print(f"The mean score using nested cross-validation is: "
+      f"{test_score.mean():.3f} +/- {test_score.std():.3f}")
 
 # %% [markdown]
 # In the example above, the reported score is more trustful and should be close
@@ -103,7 +103,9 @@ print(
 #
 # We will illustrate the difference between the nested and non-nested
 # cross-validation scores to show that the latter one will be too optimistic in
-# practice.
+# practice. In this regard, we will repeat several time the experiment and
+# shuffle the data differently. Besides, we will store the score obtain with
+# and without the nested cross-validation.
 
 # %%
 test_score_not_nested = []
@@ -116,20 +118,28 @@ for i in range(N_TRIALS):
 
     # Non_nested parameter search and scoring
     model = GridSearchCV(
-        estimator=SVC(), param_grid=param_grid, cv=inner_cv,
-        n_jobs=-1,
-    )
+        estimator=model_to_tune, param_grid=param_grid, cv=inner_cv,
+        n_jobs=-1)
     model.fit(X, y)
     test_score_not_nested.append(model.best_score_)
 
     # Nested CV with parameter optimization
-    result = cross_validate(
-        model, X, y, cv=outer_cv, n_jobs=-1,
-    )
-    test_score_nested.append(result["test_score"].mean())
+    test_score = cross_val_score(model, X, y, cv=outer_cv, n_jobs=-1)
+    test_score_nested.append(test_score.mean())
+
+# %% [markdown]
+# We can merge the dta together and make a box plot of the two strategies.
 
 # %%
 import pandas as pd
+
+all_scores = {
+    "Not nested CV": test_score_not_nested,
+    "Nested CV": test_score_nested,
+}
+all_scores = pd.DataFrame(all_scores)
+
+# %%
 import seaborn as sns
 sns.set_context("talk")
 # Define the style of the box style
@@ -141,13 +151,8 @@ boxplot_property = {
     "capprops": dict(linewidth=3, color='black', alpha=0.9),
 }
 
-df = pd.DataFrame(
-    {
-        "Not nested CV": test_score_not_nested,
-        "Nested CV": test_score_nested,
-    }
-)
-ax = df.plot.box(**boxplot_property)
+
+ax = all_scores.plot.box(**boxplot_property)
 ax.set_xlabel("Accuracy")
 _ = ax.set_title(
     "Comparison of mean accuracy obtained on the test sets with\n"
@@ -157,6 +162,12 @@ _ = ax.set_title(
 # %% [markdown]
 # We observe that the model's performance with the nested cross-validation is
 # not as good as the non-nested cross-validation.
+#
+# As a conclusion, when optimizing part of the machine learning pipeline
+# (e.g. hyperparameter, transform, etc.), one needs to use nested
+# cross-validation to evaluate the performance of the predictive model.
+# Otherwise, the results obtained without nested cross-validation are too
+# over-optimistic.
 #
 # # Take away
 # In this notebook, we presented the framework used in machine-learning to

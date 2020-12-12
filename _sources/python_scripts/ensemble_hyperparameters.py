55d39abfefb2252d1da8f14a4d8f679ad75d3da9
glemaitre <glemaitre@users.noreply.github.com>
2020-12-12 20:42:32 +0000

    deploy: 94c9c5f3ad034b5d15955e04f2528c0ba36fa1a7

--- _sources/python_scripts/ensemble_hyperparameters.py
+++ _sources/python_scripts/ensemble_hyperparameters.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# # hyperparameters tuning
+# # Hyperparameters tuning
 #
 # In the previous section, we did not discuss the parameters of random forest
 # and gradient-boosting. However, there are a couple of things to keep in mind

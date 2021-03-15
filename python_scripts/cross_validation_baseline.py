47b612bdcf53fb064e9e143226df52b06885ea32
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-03-15 16:58:07 +0100

    iter

--- python_scripts/cross_validation_baseline.py
+++ python_scripts/cross_validation_baseline.py
@@ -92,10 +92,10 @@ final_errors = pd.concat([errors_regressor, errors_dummy, errors_permutation],
 
 # %%
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-sns.displot(final_errors, kind="kde")
-_ = plt.xlabel("Mean absolute error (k$)")
+final_errors.plot.hist(bins=50, density=True, edgecolor="black")
+plt.xlabel("Mean absolute error (k$)")
+_ = plt.title("Distribution of the testing errors")
 
 # %% [markdown]
 # We see that even if the statistical performance of our model is far from

4d79a6fb5cc07141b8dd87e8d8411f03b47d41d7
Loïc Estève <loic.esteve@ymail.com>
2021-03-15 15:00:09 +0100

    Add exercise in data exploration sequence. (#251)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

---
+++ python_scripts/01_tabular_data_exploration_ex_01.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 📝 Exercise 01
+
+# %% [markdown]
+# Imagine we are interested by predicting penguins species based on two of
+# their body measurements: culmen length and culmen depth. First we want to do
+# some data exploration to get a feel for the data.
+#
+# What are the features? What is the target?
+
+# %% [markdown]
+# The data is located in `../datasets/penguins_classification.csv`, load it
+# with `pandas` into a `DataFrame`.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Show a few samples of the data
+#
+# How many features are numerical? How many features are categorical?
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# What are the different penguins species available in the dataset and how many
+# samples of each species are there? Hint: select the right column and use
+# the [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html) method.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Plot histograms for the numerical features
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Show features distribution for each class. Hint: use
+# [`seaborn.pairplot`](https://seaborn.pydata.org/generated/seaborn.pairplot.html)
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Looking at these distributions, how hard do you think it will be to classify
+# the penguins only using "culmen depth" and "culmen length"?
---
+++ python_scripts/01_tabular_data_exploration_sol_01.py
@@ -0,0 +1,85 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 📃 Solution for Exercise 01
+
+# %% [markdown]
+# Imagine we are interested by predicting penguins species based on two of
+# their body measurements: culmen length and culmen depth. First we want to do
+# some data exploration to get a feel for the data.
+#
+# What are the features? What is the target?
+
+# %% markdown
+# The features are "culmen length" and "culmen depth".
+# The target is the penguin species.
+
+# %% [markdown]
+# The data is located in `../datasets/penguins_classification.csv`, load it
+# with `pandas` into a `DataFrame`.
+
+# %%
+import pandas as pd
+
+penguins = pd.read_csv("../datasets/penguins_classification.csv")
+
+# %% [markdown]
+# Show a few samples of the data
+#
+# How many features are numerical? How many features are categorical?
+
+# %% [markdown]
+# Both features, "culmen length" and "culmen depth" are numerical. There are no
+# categorical features in this dataset.
+
+# %%
+penguins.head()
+
+# %% [markdown]
+# What are the different penguins species available in the dataset and how many
+# samples of each species are there? Hint: select the right column and use
+# the [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html) method.
+
+# %%
+penguins["Species"].value_counts()
+
+# %% [markdown]
+# Plot histograms for the numerical features
+
+# %%
+penguins.hist()
+
+# %% [markdown]
+# Show features distribution for each class. Hint: use
+# [`seaborn.pairplot`](https://seaborn.pydata.org/generated/seaborn.pairplot.html)
+
+# %%
+import seaborn
+
+seaborn.pairplot(penguins, hue="Species")
+
+# %% [markdown]
+# Looking at these distributions, how hard do you think it will be to classify
+# the penguins only using "culmen depth" and "culmen length"?
+
+# %% [markdown]
+# Looking at the previous scatter-plot showing "culmen length" and "culmen
+# depth", the species are reasonably well separated:
+# - low culmen length -> Adelie
+# - low culmen depth -> Gentoo
+# - high culmen depth and high culmen length -> Chinstrap
+#
+# There is some small overlap between the species, so we can expect a
+# statistical model to perform well on this dataset but not perfectly.

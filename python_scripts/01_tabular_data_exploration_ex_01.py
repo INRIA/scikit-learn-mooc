b3dc967cf957408855366efcb9cfe8fbf6564fee
Loïc Estève <loic.esteve@ymail.com>
2021-03-15 14:54:44 +0100

    tweak

--- python_scripts/01_tabular_data_exploration_ex_01.py
+++ python_scripts/01_tabular_data_exploration_ex_01.py
@@ -16,8 +16,9 @@
 # # 📝 Exercise 01
 
 # %% [markdown]
-# We will try to predict penguins species based on two of their body
-# measurements: culmen length and culmen depth.
+# Imagine we are interested by predicting penguins species based on two of
+# their body measurements: culmen length and culmen depth. First we want to do
+# some data exploration to get a feel for the data.
 #
 # What are the features? What is the target?
 
@@ -37,7 +38,9 @@
 # Write your code here.
 
 # %% [markdown]
-# How many samples of each species do you have?
+# What are the different penguins species available in the dataset and how many
+# samples of each species are there? Hint: select the right column and use
+# the [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html) method.
 
 # %%
 # Write your code here.
@@ -49,12 +52,12 @@
 # Write your code here.
 
 # %% [markdown]
-# Show features distribution for each class.
+# Show features distribution for each class. Hint: use
+# [`seaborn.pairplot`](https://seaborn.pydata.org/generated/seaborn.pairplot.html)
 
 # %%
 # Write your code here.
 
 # %% [markdown]
-# Looking at this distributions, how
-# hard do you think it will be to classify the penguins only using "culmen
-# depth" and "culmen length"?
+# Looking at these distributions, how hard do you think it will be to classify
+# the penguins only using "culmen depth" and "culmen length"?
--- python_scripts/01_tabular_data_exploration_sol_01.py
+++ python_scripts/01_tabular_data_exploration_sol_01.py
@@ -16,8 +16,9 @@
 # # 📃 Solution for Exercise 01
 
 # %% [markdown]
-# We will try to predict penguins species based on two of their body
-# measurements: culmen length and culmen depth.
+# Imagine we are interested by predicting penguins species based on two of
+# their body measurements: culmen length and culmen depth. First we want to do
+# some data exploration to get a feel for the data.
 #
 # What are the features? What is the target?
 
@@ -41,13 +42,15 @@ penguins = pd.read_csv("../datasets/penguins_classification.csv")
 
 # %% [markdown]
 # Both features, "culmen length" and "culmen depth" are numerical. There are no
-# no categorical features in this dataset.
+# categorical features in this dataset.
 
 # %%
 penguins.head()
 
 # %% [markdown]
-# How many samples of each species do you have?
+# What are the different penguins species available in the dataset and how many
+# samples of each species are there? Hint: select the right column and use
+# the [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html) method.
 
 # %%
 penguins["Species"].value_counts()
@@ -59,7 +62,8 @@ penguins["Species"].value_counts()
 penguins.hist()
 
 # %% [markdown]
-# Show features distribution for each class.
+# Show features distribution for each class. Hint: use
+# [`seaborn.pairplot`](https://seaborn.pydata.org/generated/seaborn.pairplot.html)
 
 # %%
 import seaborn
@@ -67,13 +71,12 @@ import seaborn
 seaborn.pairplot(penguins, hue="Species")
 
 # %% [markdown]
-# Looking at this distributions, how
-# hard do you think it will be to classify the penguins only using "culmen
-# depth" and "culmen length"?
+# Looking at these distributions, how hard do you think it will be to classify
+# the penguins only using "culmen depth" and "culmen length"?
 
 # %% [markdown]
-# Looking at the scatter-plot showing both "culmen length" and "culmen depth",
-# the species are reasonably well separated:
+# Looking at the previous scatter-plot showing "culmen length" and "culmen
+# depth", the species are reasonably well separated:
 # - low culmen length -> Adelie
 # - low culmen depth -> Gentoo
 # - high culmen depth and high culmen length -> Chinstrap

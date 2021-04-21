e60a21b603f3d975185b6c057ebd7e6caa0062c5
ogrisel <ogrisel@users.noreply.github.com>
2021-04-21 18:54:06 +0000

    deploy: 10cdb90dc8094d058e34c2dd657b82917f787e42

--- _sources/python_scripts/linear_regression_non_linear_link.py
+++ _sources/python_scripts/linear_regression_non_linear_link.py
@@ -1,13 +1,17 @@
 # %% [markdown]
-# # Linear regression with non-linear link between data and target
+# # Linear regression for a non-linear features-target relationship
 #
 # In the previous exercise, you were asked to train a linear regression model
 # on a dataset where the matrix `data` and the vector `target` do not have a
 # linear link.
 #
 # In this notebook, we show that even if the parametrization of linear models
-# is not natively adapated to data with non-linearity, it is still possible
-# to make linear model more flexible and expressive.
+# is not natively adapted to the problem at hand, it is still possible to make
+# linear models more expressive by engineering additional features.
+#
+# A machine learning pipeline that combines a non-linear feature engineering
+# step followed by a linear regression step can therefore be considered
+# non-linear regression model as a whole.
 #
 # To illustrate these concepts, we will reuse the same dataset generated in the
 # previous exercise.
@@ -27,18 +31,19 @@ target = data ** 3 - 0.5 * data ** 2 + noise
 
 # %% [markdown]
 # ```{note}
-# To ease the plotting, we will create a Pandas dataframe containing the data
-# and target
+# To ease the plotting, we will create a pandas dataframe containing the data
+# and target:
 # ```
 
 # %%
 import pandas as pd
-full_data = pd.DataFrame({"data": data, "target": target})
+
+full_data = pd.DataFrame({"input_feature": data, "target": target})
 
 # %%
 import seaborn as sns
 
-_ = sns.scatterplot(data=full_data, x="data", y="target")
+_ = sns.scatterplot(data=full_data, x="input_feature", y="target")
 # %% [markdown]
 # We will highlight the limitations of fitting a linear regression model as
 # done in the previous exercise.
@@ -52,42 +57,47 @@ _ = sns.scatterplot(data=full_data, x="data", y="target")
 # ```
 
 # %%
+# X should be 2D for sklearn: (n_samples, n_features)
+data = data.reshape((-1, 1))
+data.shape
+
+# %%
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 linear_regression = LinearRegression()
-# X should be 2D for sklearn
-data_2d = data.reshape((-1, 1))
-linear_regression.fit(data_2d, target)
-target_predicted = linear_regression.predict(data_2d)
+linear_regression.fit(data, target)
+target_predicted = linear_regression.predict(data)
 
 # %%
 mse = mean_squared_error(target, target_predicted)
 
 # %%
-ax = sns.scatterplot(data=full_data, x="data", y="target")
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
 ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
-# Here the coefficients learnt by `LinearRegression` is the best "straight
-# line" that fits the data. We can inspect the coefficients using the
-# attributes of the model learnt as follows:
+#
+# Here the coefficient and intercept learnt by `LinearRegression` define the
+# best "straight line" that fits the data. We can inspect the coefficients
+# using the attributes of the model learnt as follows:
 
 # %%
 print(f"weight: {linear_regression.coef_[0]:.2f}, "
       f"intercept: {linear_regression.intercept_:.2f}")
 
 # %% [markdown]
-# It is important to note that the model learnt will not be able to handle the
+#
+# It is important to note that the learnt model will not be able to handle the
 # non-linear relationship between `data` and `target` since linear models
 # assume the relationship between `data` and `target` to be linear.
 #
 # Indeed, there are 3 possibilities to solve this issue:
 #
-# 1. choose a model that natively can deal with non-linearity,
-# 2. "augment" features by including expert knowledge which can be used by
-#    the model, or
+# 1. choose a model that can natively deal with non-linearity,
+# 2. engineer a richer set of features by including expert knowledge which can
+#    be directly used by a simple linear model, or
 # 3. use a "kernel" to have a locally-based decision function instead of a
 #    global linear decision function.
 #
@@ -97,28 +107,38 @@ print(f"weight: {linear_regression.coef_[0]:.2f}, "
 # %%
 from sklearn.tree import DecisionTreeRegressor
 
-tree = DecisionTreeRegressor(max_depth=3).fit(data_2d, target)
-target_predicted = tree.predict(data_2d)
+tree = DecisionTreeRegressor(max_depth=3).fit(data, target)
+target_predicted = tree.predict(data)
 mse = mean_squared_error(target, target_predicted)
 
 # %%
-ax = sns.scatterplot(data=full_data, x="data", y="target")
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
 ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
+#
 # Instead of having a model which can natively deal with non-linearity, we
 # could also modify our data: we could create new features, derived from the
 # original features, using some expert knowledge. In this example, we know that
 # we have a cubic and squared relationship between `data` and `target` (because
-# we generated the data). Indeed, we could create two new features (`data ** 2`
-# and `data ** 3`) using this information.
+# we generated the data).
+#
+# Indeed, we could create two new features (`data ** 2` and `data ** 3`) using
+# this information as follows. This kind of transformation is called a
+# polynomial feature expansion:
 
 # %%
-data_augmented = np.concatenate([data_2d, data_2d ** 2, data_2d ** 3], axis=1)
+data.shape
 
-linear_regression.fit(data_augmented, target)
-target_predicted = linear_regression.predict(data_augmented)
+# %%
+data_expanded = np.concatenate([data, data ** 2, data ** 3], axis=1)
+data_expanded.shape
+
+
+# %%
+linear_regression.fit(data_expanded, target)
+target_predicted = linear_regression.predict(data_expanded)
 mse = mean_squared_error(target, target_predicted)
 
 # %%
@@ -127,76 +147,133 @@ ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
+#
 # We can see that even with a linear model, we can overcome the linearity
-# limitation of the model by adding the non-linear component into the design of
+# limitation of the model by adding the non-linear components in the design of
 # additional features. Here, we created new features by knowing the way the
-# target was generated. In practice, this is usually not the case.
+# target was generated.
 #
-# Instead, one is usually creating interaction between features (e.g. $x_1
-# \times x_2$) with different orders (e.g. $x_1, x_1^2, x_1^3$), at the risk of
-# creating a model with too much flexibility where the polynomial terms allows
-# to fit noise in the dataset and thus lead overfit. In scikit-learn, the
-# `PolynomialFeatures` is a transformer to create such feature interactions
-# which we could have used instead of manually creating new features.
+# Instead of manually creating such polynomial features on could directly use
+# [sklearn.preprocessing.PolynomialFeatures](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html).
 #
-# To demonstrate `PolynomialFeatures`, we are going to use a scikit-learn
-# pipeline which will first create the new features and then fit the model.
-# We come back to scikit-learn pipelines and discuss them in more detail later.
+# To demonstrate the use of the `PolynomialFeatures` class, we use a
+# scikit-learn pipeline which first transforms the features and then fit the
+# regression model.
 
 # %%
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
-model = make_pipeline(PolynomialFeatures(degree=3),
-                      LinearRegression())
-model.fit(data_2d, target)
-target_predicted = model.predict(data_2d)
+polynomial_regression = make_pipeline(
+      PolynomialFeatures(degree=3),
+      LinearRegression(),
+)
+polynomial_regression.fit(data, target)
+target_predicted = polynomial_regression.predict(data)
 mse = mean_squared_error(target, target_predicted)
 
 # %%
-ax = sns.scatterplot(data=full_data, x="data", y="target")
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
 ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
-# Thus, we saw that `PolynomialFeatures` is actually doing the same
-# operation that we did manually above.
+#
+# As expected, we observe that the predictions of the this `PolynomialFeatures`
+# pipeline match the predictions of the linear model fit on manually engineered
+# features.
 #
 # The last possibility is to make a linear model more expressive is to use a
 # "kernel". Instead of learning a weight per feature as we previously
 # emphasized, a weight will be assign by sample instead. However, not all
 # samples will be used. This is the base of the support vector machine
 # algorithm.
+#
+# The mathematical definition of kernel-based support vector machines is beyond
+# the scope of this course. We encourage interested readers to have a look at
+# the scikit-learn [documentation on
+# SVMs](https://scikit-learn.org/stable/modules/svm.html) for more details.
+#
+# For the rest of us, let's just develop some intuitions on their expressive
+# power by trying them on the same dataset:
 
 # %%
 from sklearn.svm import SVR
 
 svr = SVR(kernel="linear")
-svr.fit(data_2d, target)
-target_predicted = svr.predict(data_2d)
+svr.fit(data, target)
+target_predicted = svr.predict(data)
 mse = mean_squared_error(target, target_predicted)
 
 # %%
-ax = sns.scatterplot(data=full_data, x="data", y="target")
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
 ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
-# The algorithm can be modified so that it can use non-linear kernel. Then,
-# it will compute interaction between samples using this non-linear
-# interaction.
+#
+# The estimator can be configured to use a non-linear kernel. Then, it can
+# learn a prediction function that computes non-linear interaction between
+# samples to predict and selected samples from the training set.
+#
+# The result is another kind of non-linear regression model with a similar
+# expressivity are our previous polynomial regression pipeline:
 
 # %%
 svr = SVR(kernel="poly", degree=3)
-svr.fit(data_2d, target)
-target_predicted = svr.predict(data_2d)
+svr.fit(data, target)
+target_predicted = svr.predict(data)
 mse = mean_squared_error(target, target_predicted)
 
 # %%
-ax = sns.scatterplot(data=full_data, x="data", y="target")
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
 ax.plot(data, target_predicted, color="tab:orange")
 _ = ax.set_title(f"Mean squared error = {mse:.2f}")
 
 # %% [markdown]
-# A method supporting kernel, as SVM, allows to efficiently create a non-linear
-# model.
+#
+# Kernel methods such as SVR are very efficient for small to medium datasets.
+#
+# For larger datasets with `n_samples >> 10_000`, it is often computationally
+# more efficient to perform explicit feature expansion using
+# `PolynomialFeatures` or other non-linear transformers from scikit-learn such
+# as
+# [KBinsDiscretizer](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.KBinsDiscretizer.html)
+# or
+# [Nystroem](https://scikit-learn.org/stable/modules/generated/sklearn.kernel_approximation.Nystroem.html).
+#
+# Here again we refer the interested reader to the documentation to get a
+# proper definition of those methods. The following just gives an intuitive
+# overview of the predictions we would get using those on our toy dataset:
+
+# %%
+from sklearn.preprocessing import KBinsDiscretizer
+
+binned_regression = make_pipeline(
+      KBinsDiscretizer(n_bins=8),
+      LinearRegression(),
+)
+binned_regression.fit(data, target)
+target_predicted = binned_regression.predict(data)
+mse = mean_squared_error(target, target_predicted)
+
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
+ax.plot(data, target_predicted, color="tab:orange")
+_ = ax.set_title(f"Mean squared error = {mse:.2f}")
+
+# %%
+from sklearn.kernel_approximation import Nystroem
+
+nystroem_regression = make_pipeline(
+      Nystroem(n_components=5),
+      LinearRegression(),
+)
+nystroem_regression.fit(data, target)
+target_predicted = nystroem_regression.predict(data)
+mse = mean_squared_error(target, target_predicted)
+
+ax = sns.scatterplot(data=full_data, x="input_feature", y="target")
+ax.plot(data, target_predicted, color="tab:orange")
+_ = ax.set_title(f"Mean squared error = {mse:.2f}")
+
+# %%

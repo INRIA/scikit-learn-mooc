dbc5b3ea0a4898074546cf07082774272802d302
Loïc Estève <loic.esteve@ymail.com>
2021-04-28 07:53:14 +0200

    Make cross-validation graph more readable.
    - make legend and axes text more readable by using matplotlibrc
    - make plot smaller by using 50 samples
    - blacken the script

--- figures/plot_cross_validation_diagram.py
+++ figures/plot_cross_validation_diagram.py
@@ -2,13 +2,15 @@ from pathlib import Path
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Patch
 
 
 FIGURES_FOLDER = Path(__file__).parent
 cmap_cv = plt.cm.coolwarm
 
+plt.style.use(FIGURES_FOLDER / "../python_scripts/matplotlibrc")
 
+
+# +
 def plot_cv_indices(cv, X, y, ax, lw=50):
     """Create a sample plot for indices of a cross-validation object."""
     splits = list(cv.split(X=X, y=y))
@@ -21,26 +23,38 @@ def plot_cv_indices(cv, X, y, ax, lw=50):
         indices[train] = 1
 
         # Visualize the results
-        ax.scatter(range(len(indices)), [ii + .5] * len(indices),
-                   c=indices, marker='_', lw=lw, cmap=cmap_cv,
-                   vmin=-.2, vmax=1.2)
+        ax.scatter(
+            range(len(indices)),
+            [ii + 0.5] * len(indices),
+            c=indices,
+            marker="_",
+            lw=25,
+            cmap=cmap_cv,
+            vmin=-0.2,
+            vmax=1.2,
+        )
 
     # Formatting
     yticklabels = list(range(n_splits))
-    ax.set(yticks=np.arange(n_splits) + .5,
-           yticklabels=yticklabels, xlabel='Sample index',
-           ylabel="CV iteration", ylim=[n_splits + .2,
-                                        -.2], xlim=[0, 100])
-    ax.set_title('{}'.format(type(cv).__name__), fontsize=15)
+    ax.set(
+        yticks=np.arange(n_splits) + 0.5,
+        yticklabels=yticklabels,
+        xlabel="Sample index",
+        ylabel="CV iteration",
+        ylim=[n_splits + 0.2, -0.2],
+        xlim=[0, 50],
+    )
+    ax.set_title("{}".format(type(cv).__name__))
     return ax
 
 
-n_points = 100
+n_points = 50
 X = np.random.randn(n_points, 10)
 y = np.random.randn(n_points)
 
-fig, ax = plt.subplots(figsize=(20, 8))
+fig, ax = plt.subplots(figsize=(10, 4))
 cv = KFold(5)
 _ = plot_cv_indices(cv, X, y, ax)
-fig.savefig(FIGURES_FOLDER / "cross_validation_diagram.png")
 plt.tight_layout()
+fig.savefig(FIGURES_FOLDER / "cross_validation_diagram.png")
+# -

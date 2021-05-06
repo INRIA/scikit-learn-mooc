7d84cbd1e79a5850442fccf34852430ef7b5c8c9
Loïc Estève <loic.esteve@ymail.com>
2021-05-06 07:36:58 +0200

    Show failure logs after jupyter-book detected failure.

--- build_tools/circle/build_jupyter_book.sh
+++ build_tools/circle/build_jupyter_book.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -xe
 
+function show_error_logs {
+    echo "Some notebooks failed, see logs below:"
+    for f in _build/html/reports/*.log; do
+        echo "================================================================================"
+        echo $f
+        echo "================================================================================"
+        cat $f
+    done
+    # You need to exit with non-zero here to cause the build to fail
+    exit 1
+}
+
 apt-get install make
 
 source /opt/conda/etc/profile.d/conda.sh
@@ -14,5 +26,5 @@ make 2>&1 | tee build.log
 
 # Grep the log to make sure there has been no errors when running the notebooks
 # since jupyter-book exit code is always 0
-grep 'Execution Failed' build.log && exit 1 || \
+grep 'Execution Failed' build.log && show_error_logs || \
     echo 'All notebooks ran successfully'

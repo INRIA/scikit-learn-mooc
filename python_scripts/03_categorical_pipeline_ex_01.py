dd18ce52b0a816efb7f4841d12f30ed42bc7b6bb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-05-06 14:16:18 +0200

    ENH improve directive in exercise M1.04

--- python_scripts/03_categorical_pipeline_ex_01.py
+++ python_scripts/03_categorical_pipeline_ex_01.py
@@ -22,12 +22,10 @@
 # To do so, let's try to use `OrdinalEncoder` to preprocess the categorical
 # variables. This preprocessor is assembled in a pipeline with
 # `LogisticRegression`. The statistical performance of the pipeline can be
-# evaluated as usual by cross-validation and then compared to the score
-# obtained when using `OneHotEncoder` or to some other baseline score.
+# evaluated by cross-validation and then compared to the score obtained when
+# using `OneHotEncoder` or to some other baseline score.
 #
-# Because `OrdinalEncoder` can raise errors if it sees an unknown category at
-# prediction time, you can set the `handle_unknown` and `unknown_value`
-# parameters.
+# First, we load the dataset.
 
 # %%
 import pandas as pd
@@ -40,7 +38,11 @@ target = adult_census[target_name]
 data = adult_census.drop(columns=[target_name, "education-num"])
 
 # %% [markdown]
-# We can select the categorical based on the `object` dtype.
+# In the previous notebook, we used `sklearn.compose.make_column_selector` to
+# automatically select columns with a specific data type (also called `dtype`).
+# Here, we will use this selector to get only the columns containing strings
+# (column with `object` dtype) that corresponds to categorical features in our
+# dataset.
 
 # %%
 from sklearn.compose import make_column_selector as selector
@@ -49,10 +51,41 @@ categorical_columns_selector = selector(dtype_include=object)
 categorical_columns = categorical_columns_selector(data)
 data_categorical = data[categorical_columns]
 
+# %% [markdown]
+# Now, that we have our dataset with only categorical features, you will
+# define a scikit-learn pipeline composed of an `OrdinalEncoder` and a
+# `LogisticRegression` classifier.
+#
+# Because `OrdinalEncoder` can raise errors if it sees an unknown category at
+# prediction time, you can set the `handle_unknown="use_encoded_value"` and
+# `unknown_value` parameters. You can refer to the
+# [scikit-learn documentation](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OrdinalEncoder.html)
+# for more details regarding these parameters.
+
 # %%
-from sklearn.model_selection import cross_validate
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.linear_model import LogisticRegression
+
+# Write your code here.
+
+# %% [markdown]
+# Now that your model is created, evaluate it using a cross-validation using
+# `sklearn.model_selection.cross_validate`.
+
+# %%
+from sklearn.model_selection import cross_validate
+
+# Write your code here.
+
+# %% [markdown]
+# Now, we would like to compare the statistical performance of our previous
+# model with a new model where instead of using an `OrdinalEncoder`, we will
+# use a `OneHotEncoder`. Repeat the model evaluation using cross-validation.
+# Compare the score of both models and conclude on the impact of the choosing a
+# specific encoding strategy when using a linear model.
+
+# %%
+from sklearn.preprocessing import OneHotEncoder
 
 # Write your code here.

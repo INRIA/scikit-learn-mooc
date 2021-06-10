992f8bba0bc1f75d62e72b444bde94b517d71965
lesteve <lesteve@users.noreply.github.com>
2021-06-10 17:33:52 +0000

    deploy: fe03783de404a5e7ff496c9da75600e0abec4504

--- _sources/python_scripts/parameter_tuning_sol_03.py
+++ _sources/python_scripts/parameter_tuning_sol_03.py
@@ -23,7 +23,6 @@
 # is powerful enough.
 
 # %%
-
 import numpy as np
 import pandas as pd
 
@@ -70,8 +69,8 @@ numerical_processor = StandardScaler()
 from sklearn.compose import ColumnTransformer
 
 preprocessor = ColumnTransformer(
-    [('cat_preprocessor', categorical_processor, categorical_columns),
-     ('num_preprocessor', numerical_processor, numerical_columns)]
+    [('cat-preprocessor', categorical_processor, categorical_columns),
+     ('num-preprocessor', numerical_processor, numerical_columns)]
 )
 
 # %% [markdown]
@@ -104,8 +103,8 @@ from scipy.stats import loguniform
 
 param_distributions = {
     "logisticregression__C": loguniform(0.001, 10),
-    "columntransformer__num_preprocessor__with_mean": [True, False],
-    "columntransformer__num_preprocessor__with_std": [True, False],
+    "columntransformer__num-preprocessor__with_mean": [True, False],
+    "columntransformer__num-preprocessor__with_std": [True, False],
 }
 
 model_random_search = RandomizedSearchCV(
@@ -142,8 +141,8 @@ cv_results = pd.read_csv(
 # %%
 column_name_mapping = {
     "param_logisticregression__C": "C",
-    "param_columntransformer__num_preprocessor__with_mean": "centering",
-    "param_columntransformer__num_preprocessor__with_std": "scaling",
+    "param_columntransformer__num-preprocessor__with_mean": "centering",
+    "param_columntransformer__num-preprocessor__with_std": "scaling",
     "mean_test_score": "mean test accuracy",
 }
 

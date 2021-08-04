46d7903e59a7fc65d3f5e4e5d4fb566234454569
ArturoAmor <86408019+ArturoAmorQ@users.noreply.github.com>
2021-08-04 16:52:52 +0200

    Split linear models exercise into separate questions (#431)

--- python_scripts/linear_models_ex_03.py
+++ python_scripts/linear_models_ex_03.py
@@ -29,17 +29,32 @@ target *= 100  # rescale the target in k$
 data.head()
 
 # %% [markdown]
-# Now this is your turn to train a linear regression model on this dataset.
-# You will need to:
-# * create a linear regression model;
-# * execute a cross-validation with 10 folds and use the mean absolute error
-#   (MAE) as metric. Ensure to return the fitted estimators;
-# * compute mean and std of the MAE in thousands of dollars (k$);
-# * show the values of the coefficients for each feature using a boxplot by
-#   inspecting the fitted model returned from the cross-validation. Hint: you
-#   use the function
-#   [`df.plot.box()`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.box.html)
-#   to plot a box plot.
+# Now it is your turn to train a linear regression model on this dataset.
+# First, create a linear regression model.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Execute a cross-validation with 10 folds and use the mean absolute error
+# (MAE) as metric. Be sure to *return* the fitted *estimators*.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Compute the mean and std of the MAE in thousands of dollars (k$).
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Inspect the fitted model using a box plot to show the distribution of values
+# for the coefficients returned from the cross-validation. Hint:
+# use the function
+# [`df.plot.box()`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.box.html)
+# to create a box plot.
+
 
 # %%
 # Write your code here.

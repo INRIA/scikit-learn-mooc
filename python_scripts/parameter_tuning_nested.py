e997dab38903c867f6a17cbdde867dc0798a906c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-09-17 17:19:01 +0200

    ENH enhanced nested cross-validation in the parameter search

--- python_scripts/parameter_tuning_nested.py
+++ python_scripts/parameter_tuning_nested.py
@@ -1,46 +1,43 @@
 # %% [markdown]
-# # Cross-validation and hyperparameter tuning
+# # Evaluation and hyperparameter tuning
 #
-# In the previous notebooks, we saw two approaches to tune hyperparameters:
-# via grid-search and randomized-search.
+# In the previous notebook, we saw two approaches to tune hyperparameters.
+# However, we did not present a proper framework to evaluate the tuned models.
+# Instead, we focused on the mechanism used to find the best set of parameters.
 #
-# In this notebook, we will show how to combine such hyperparameters search
-# with a cross-validation.
-
-# %% [markdown]
-# ## Our predictive model
+# In this notebook, we will reuse some knowledge presented in the module
+# "Selecting the best model" to show how to evaluate models where
+# hyperparameters need to be tuned.
 #
-# Let us reload the dataset as we did previously:
-
-# %%
-from sklearn import set_config
-
-set_config(display="diagram")
+# Thus, we will first load the dataset and create the predictive model that
+# we want to optimize and later on evaluate.
+#
+# ## Loading the dataset
+#
+# As in the previous notebook, we will load the Adult census dataset. The
+# loaded dataframe will be split to get the data and the target into two
+# separated variables. In addition, we will drop the column `"education-num"`
+# as previously done.
 
 # %%
 import pandas as pd
 
-adult_census = pd.read_csv("../datasets/adult-census.csv")
-
-# %% [markdown]
-# We extract the column containing the target.
-
-# %%
 target_name = "class"
+adult_census = pd.read_csv("../datasets/adult-census.csv")
 target = adult_census[target_name]
-target
+data = adult_census.drop(columns=[target_name, "education-num"])
 
 # %% [markdown]
-# We drop from our data the target and the `"education-num"` column which
-# duplicates the information from the `"education"` column.
+# ## Our predictive model
+#
+# We will now create the predictive model that we want to optimize. Note that
+# this pipeline is identical to the one we used in the previous notebook.
 
 # %%
-data = adult_census.drop(columns=[target_name, "education-num"])
-data.head()
+from sklearn import set_config
 
-# %% [markdown]
-# We will create the same predictive pipeline as seen in the grid-search
-# section.
+# To get a diagram visualization of the pipeline
+set_config(display="diagram")
 
 # %%
 from sklearn.compose import ColumnTransformer
@@ -69,56 +66,155 @@ model = Pipeline([
 model
 
 # %% [markdown]
-# ## Include a hyperparameter search within a cross-validation
+# ## Evaluation
 #
-# As mentioned earlier, using a single train-test split during the grid-search
-# does not give any information regarding the different sources of variations:
-# variations in terms of test score or hyperparameters values.
+# ### Without hyperparameter tuning
 #
-# To get reliable information, the hyperparameters search need to be nested
-# within a cross-validation.
-#
-# ```{note}
-# To limit the computational cost, we affect `cv` to a low integer. In
-# practice, the number of fold should be much higher.
-# ```
+# In the module "Selecting the best model", we saw that one must use
+# cross-validation to evaluate such a model. Cross-validation allows to get
+# a distribution of the scores of the model. Thus, having this distribution at
+# hand, we can get the variability of the generalization performance of the
+# model. Here, we recall the necessary `scikit-learn` functionality to use
+# to obtain the mean and standard deviation of the scores. We will use the
+# `cross_validate` function to evaluate our model.
 
 # %%
 from sklearn.model_selection import cross_validate
+
+cv_results = cross_validate(model, data, target, cv=5)
+cv_results = pd.DataFrame(cv_results)
+cv_results
+
+# %%
+# The cross-validation scores are coming from a 5-fold cross-validation. So
+# we can compute the mean and standard deviation of the generalization score.
+
+# %%
+print(
+    "Generalization score without hyperparameters tuning:\n"
+    f"{cv_results['test_score'].mean():.3f} +/- {cv_results['test_score'].std():.3f}"
+)
+
+# %% [markdown]
+# Now, we will present how to evaluate the model with hyperparameter tuning,
+# where an extra step is required to select the best set of parameters.
+#
+# ### With hyperparameter tuning
+#
+# As we shown in the previous notebook, one can use a search strategy that uses
+# cross-validation to find the best set of parameters. Here, we will use a
+# grid-search strategy and reproduce the steps done in the previous notebook.
+#
+# First, we have to embed our model into a grid-search and specify the
+# parameters and the parameter values that we want to explore.
+
+# %%
 from sklearn.model_selection import GridSearchCV
 
 param_grid = {
-    'classifier__learning_rate': (0.05, 0.1),
-    'classifier__max_leaf_nodes': (30, 40)}
-model_grid_search = GridSearchCV(model, param_grid=param_grid,
-                                 n_jobs=2, cv=2)
+    'classifier__learning_rate': (0.05, 0.5),
+    'classifier__max_leaf_nodes': (10, 30)}
+model_grid_search = GridSearchCV(
+    model, param_grid=param_grid, n_jobs=2, cv=2
+)
+model_grid_search.fit(data, target)
 
+# %% [markdown]
+# As previously saw, when calling the `fit` method, the model embedded in the
+# grid-search will be trained with every possible combinations of parameters
+# resulting from the parameter grid. The best combination was selected by
+# keeping the combination of the best mean accuracy score.
+
+# %%
+cv_results = pd.DataFrame(model_grid_search.cv_results_)
+cv_results[[
+    "param_classifier__learning_rate",
+    "param_classifier__max_leaf_nodes",
+    "mean_test_score",
+    "rank_test_score"
+]]
+
+# %%
+model_grid_search.best_params_
+
+# %% [markdown]
+# One important bottleneck here concerns the generalization performance.
+# Indeed, the mean and standard deviation of the generalization score provided
+# by the cross-validation in the grid-search is potentially not a good estimate
+# of the generalization performance of the model refitted with the best
+# parameters (i.e. the model at hand when using `model_grid_search.predict`).
+#
+# The reason is that the model was refitted on the full dataset given during
+# `fit`. Therefore, this refitted model was trained with more data than the
+# different models trained during the cross-validation in the grid-search.
+#
+# Therefore, one must keep a test set to evaluate the refitted model. We
+# highlight here the process using a single train-test split.
+
+# %%
+from sklearn.model_selection import train_test_split
+
+data_train, data_test, target_train, target_test = train_test_split(
+    data, target, test_size=0.2, random_state=42
+)
+
+model_grid_search.fit(data_train, target_train)
+accuracy = model_grid_search.score(data_test, target_test)
+print(f"Accuracy on test set: {accuracy:.3f}")
+
+# %% [markdown]
+# In the code above, the selection of the best hyperparameters was done only on
+# the train set. Then, we evaluated the generalization performance of our
+# tuned model on the left out test set.
+#
+# However, this evaluation only provides us a single point estimate of the
+# generalization performance. As recall at the beginning of this notebook,
+# it is beneficial to have an uncertainty measure of the generalization
+# performance. Therefore, we should instead use a cross-validation for this
+# evaluation.
+#
+# This pattern is called *nested cross-validation*. We use an inner
+# cross-validation for the selection of the hyperparameters and an outer
+# cross-validation for the evaluation of generalization performance of the
+# refitted tuned model.
+#
+# In practice, we only need to embed the grid-search in the function
+# `cross-validate` to perform such evaluation.
+
+# %%
 cv_results = cross_validate(
-    model_grid_search, data, target, cv=3, return_estimator=True)
-
-# %% [markdown]
-# Running the above cross-validation will give us an estimate of the
-# testing score.
+    model_grid_search, data, target, cv=5, n_jobs=2, return_estimator=True
+)
 
 # %%
-scores = cv_results["test_score"]
-print(f"Accuracy score by cross-validation combined with hyperparameters "
-      f"search:\n{scores.mean():.3f} +/- {scores.std():.3f}")
+cv_results = pd.DataFrame(cv_results)
+print(
+    "Generalization score with hyperparameters tuning:\n"
+    f"{cv_results['test_score'].mean():.3f} +/- {cv_results['test_score'].std():.3f}"
+)
 
 # %% [markdown]
-# The hyperparameters on each fold are potentially different since we nested
-# the grid-search in the cross-validation. Thus, checking the variation of the
-# hyperparameters across folds should also be analyzed.
+# In this case, we obtain a distribution of scores and therefore, we can
+# apprehend the variability of the generalization performance.
+#
+# In addition, passing the parameter `return_estimator=True`, we can check the
+# value of the best hyperparameters for each fold of the outer
+# cross-validation.
 
 # %%
-for fold_idx, estimator in enumerate(cv_results["estimator"]):
-    print(f"Best parameter found on fold #{fold_idx + 1}")
-    print(f"{estimator.best_params_}")
+for cv_fold, estimator_in_fold in enumerate(cv_results["estimator"]):
+    print(
+        f"Best hyperparameters for fold #{cv_fold + 1}:\n"
+        f"{estimator_in_fold.best_params_}"
+    )
 
 # %% [markdown]
+# Such an analysis allows to see if only a single set of hyperparameters is
+# sufficient to obtain a good generalization performance or if there is some
+# variation in the hyperparameters used.
+#
 # Obtaining models with unstable hyperparameters would be an issue in practice.
 # Indeed, it would become difficult to set them.
-
-# %% [markdown]
+#
 # In this notebook, we have seen how to combine hyperparameters search with
 # cross-validation.

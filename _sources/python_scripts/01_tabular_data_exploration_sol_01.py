1d731694b46c297e3590af711f4d4c8f99e45533
ArturoAmorQ <ArturoAmorQ@users.noreply.github.com>
2022-02-03 13:40:26 +0000

    [ci skip] Tabular data tweaks (#560)
    * tweaks in First look at our dataset nb
    
    * tweaks in sol nb M1.01
    
    * Undo unnecessary black formating
    
    * tweaks in ex nb M1.01
    
    * iter 1db38a192e514d234e1b8826d10ddb4e2ad765b2

--- _sources/python_scripts/01_tabular_data_exploration_sol_01.py
+++ _sources/python_scripts/01_tabular_data_exploration_sol_01.py
@@ -16,19 +16,19 @@
 # # 📃 Solution for Exercise M1.01
 
 # %% [markdown]
-# Imagine we are interested in predicting penguins species based on two of
-# their body measurements: culmen length and culmen depth. First we want to do
-# some data exploration to get a feel for the data.
+# Imagine we are interested in predicting penguins species based on two of their
+# body measurements: culmen length and culmen depth. First we want to do some
+# data exploration to get a feel for the data.
 #
 # What are the features? What is the target?
 
 # %% [markdown] tags=["solution"]
-# The features are "culmen length" and "culmen depth".
-# The target is the penguin species.
+# The features are `"culmen length"` and `"culmen depth"`. The target is the
+# penguin species.
 
 # %% [markdown]
-# The data is located in `../datasets/penguins_classification.csv`, load it
-# with `pandas` into a `DataFrame`.
+# The data is located in `../datasets/penguins_classification.csv`, load it with
+# `pandas` into a `DataFrame`.
 
 # %%
 # solution
@@ -37,13 +37,13 @@ import pandas as pd
 penguins = pd.read_csv("../datasets/penguins_classification.csv")
 
 # %% [markdown]
-# Show a few samples of the data
+# Show a few samples of the data.
 #
 # How many features are numerical? How many features are categorical?
 
 # %% [markdown] tags=["solution"]
-# Both features, "culmen length" and "culmen depth" are numerical. There are no
-# categorical features in this dataset.
+# Both features, `"culmen length"` and `"culmen depth"` are numerical. There are
+# no categorical features in this dataset.
 
 # %%
 # solution
@@ -51,8 +51,9 @@ penguins.head()
 
 # %% [markdown]
 # What are the different penguins species available in the dataset and how many
-# samples of each species are there? Hint: select the right column and use
-# the [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html) method.
+# samples of each species are there? Hint: select the right column and use the
+# [`value_counts`](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html)
+# method.
 
 # %%
 # solution
@@ -76,21 +77,20 @@ import seaborn
 pairplot_figure = seaborn.pairplot(penguins, hue="Species")
 
 # %% [markdown] tags=["solution"]
-# We observe that the labels on the axis are overlapping. Even if it is not
-# the priority of this notebook, one can tweak the by increasing the height
-# of each subfigure.
+# We observe that the labels on the axis are overlapping. Even if it is not the
+# priority of this notebook, one can tweak them by increasing the height of each
+# subfigure.
 
 # %% tags=["solution"]
-pairplot_figure = seaborn.pairplot(
-    penguins, hue="Species", height=4)
+pairplot_figure = seaborn.pairplot(penguins, hue="Species", height=4)
 
 # %% [markdown]
 # Looking at these distributions, how hard do you think it will be to classify
-# the penguins only using "culmen depth" and "culmen length"?
+# the penguins only using `"culmen depth"` and `"culmen length"`?
 
 # %% [markdown] tags=["solution"]
-# Looking at the previous scatter-plot showing "culmen length" and "culmen
-# depth", the species are reasonably well separated:
+# Looking at the previous scatter-plot showing `"culmen length"` and `"culmen
+# depth"`, the species are reasonably well separated:
 # - low culmen length -> Adelie
 # - low culmen depth -> Gentoo
 # - high culmen depth and high culmen length -> Chinstrap

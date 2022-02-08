f134c7f1b23da7577eeb1454b7e911b53a5337f6
Loïc Estève <loic.esteve@ymail.com>
2022-02-08 18:38:54 +0100

    Further fix to content_only logic

--- jupyter-book/_static/sklearn_mooc.js
+++ jupyter-book/_static/sklearn_mooc.js
@@ -22,6 +22,11 @@
         removeIfExists(document.querySelector('#site-navigation'));
         removeIfExists(document.querySelector('.topbar'));
         removeIfExists(document.querySelector('.footer'));
+        // the prev/next buttons at the bottom of the page may have a different
+        // class (depending on the theme version maybe?), removing both to be
+        // safe.
+        removeIfExists(document.querySelector('.prev-next-bottom'));
+        removeIfExists(document.querySelector('.prev-next-area'));
         var elementsToRemove = document.querySelectorAll('.remove-from-content-only');
         elementsToRemove.forEach(
             function(el) {
@@ -31,7 +36,9 @@
         document.querySelector('#main-content').querySelector('.col-md-9').className = 'col-12';
 
         var style = document.createElement('style');
-        style.appendChild(document.createTextNode('hypothesis-sidebar, hypothesis-notebook, hypothesis-adder{display:none!important;}'));
+        style.appendChild(
+            document.createTextNode(
+                'hypothesis-sidebar, hypothesis-notebook, hypothesis-adder{display:none!important;}'));
         document.getElementsByTagName('head')[0].appendChild(style);
     }
 

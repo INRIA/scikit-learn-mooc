3d91531a8d9bccfaaac813ac43eca79cd3519e8c
Loïc Estève <loic.esteve@ymail.com>
2022-02-08 18:04:26 +0100

    fix content_only js

--- jupyter-book/_static/sklearn_mooc.js
+++ jupyter-book/_static/sklearn_mooc.js
@@ -1,36 +1,43 @@
-(function(){
-  function inIframe() {
-    try {
-      return window.self !== window.top;
-    } catch (e) {
-      return true;
+(function() {
+    function inIframe() {
+        try {
+            return window.self !== window.top;
+        } catch (e) {
+            return true;
+        }
     }
-  }
 
-  function contentOnly() {
-    var urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('content_only') !== null;
-  }
-
-  function displayContentOnly() {
-    document.querySelector('#site-navigation').remove();
-    document.querySelector('.topbar').remove();
-    document.querySelector('.prev-next-bottom').remove();
-    document.querySelector('.footer').remove();
-    var elementsToRemove = document.querySelectorAll('.remove-from-content-only');
-    elementsToRemove.forEach(
-      function(el) { el.remove(); }
-    );
-    document.querySelector('#main-content').querySelector('.col-md-9').className = 'col-12';
-
-    var style=document.createElement('style');
-    style.appendChild(document.createTextNode('hypothesis-sidebar, hypothesis-notebook, hypothesis-adder{display:none!important;}'));
-    document.getElementsByTagName('head')[0].appendChild(style);
-  }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    if (inIframe() || contentOnly()) {
-      displayContentOnly();
+    function contentOnly() {
+        var urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('content_only') !== null;
     }
-  });
+
+    function removeIfExists(el) {
+        if (el) {
+            el.remove();
+        };
+    }
+
+    function displayContentOnly() {
+        removeIfExists(document.querySelector('#site-navigation'));
+        removeIfExists(document.querySelector('.topbar'));
+        removeIfExists(document.querySelector('.footer'));
+        var elementsToRemove = document.querySelectorAll('.remove-from-content-only');
+        elementsToRemove.forEach(
+            function(el) {
+                removeIfExists(el);
+            }
+        );
+        document.querySelector('#main-content').querySelector('.col-md-9').className = 'col-12';
+
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode('hypothesis-sidebar, hypothesis-notebook, hypothesis-adder{display:none!important;}'));
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        if (inIframe() || contentOnly()) {
+            displayContentOnly();
+        }
+    });
 }());

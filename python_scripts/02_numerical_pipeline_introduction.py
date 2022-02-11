72d4d4edcfc1178764bd0e47fb51459156b21ebb
Loïc Estève <loic.esteve@ymail.com>
2022-02-11 12:16:24 +0100

    Update 02_numerical_pipeline_introduction.py

--- python_scripts/02_numerical_pipeline_introduction.py
+++ python_scripts/02_numerical_pipeline_introduction.py
@@ -92,7 +92,7 @@ print(f"The dataset contains {data.shape[0]} samples and "
 # The `fit` method is called to train the model from the input (features) and
 # target data.
 
-# %%
+# %%/
 # to display nice model diagram
 from sklearn import set_config
 set_config(display='diagram')
@@ -172,10 +172,10 @@ print(f"Number of correct prediction: "
 (target == target_predicted).mean()
 
 # %% [markdown]
-# This result means that the model makes a correct _prediction_ for
-# approximately 82 samples out of 100. But, can this really be called a _prediction_ if we are predicting 
-# already learned data? In other words, can this evaluation be trusted, or is it
-# too good to be true?
+# This result means that the model makes a correct prediction for
+# approximately 82 samples out of 100. Note that we used the same data
+# to train and evaluate our model. Can this evaluation be trusted or is
+# it to good to be true?
 #
 # ## Train-test data split
 #

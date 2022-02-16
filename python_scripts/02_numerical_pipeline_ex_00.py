7ed8527eb520c5af795c349354ef878fb4c71528
Loïc Estève <loic.esteve@ymail.com>
2022-02-16 14:00:12 +0100

    Regenerate exercises with jupytext header

--- python_scripts/02_numerical_pipeline_ex_00.py
+++ python_scripts/02_numerical_pipeline_ex_00.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3

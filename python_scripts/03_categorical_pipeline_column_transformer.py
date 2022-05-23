2d67c36033d66124a6571dd8574ef8f74925a340
Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>
2022-05-23 16:21:30 +0200

    Update python_scripts/03_categorical_pipeline_column_transformer.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- python_scripts/03_categorical_pipeline_column_transformer.py
+++ python_scripts/03_categorical_pipeline_column_transformer.py
@@ -152,8 +152,10 @@ data_train, data_test, target_train, target_test = train_test_split(
 #
 # ```{caution}
 # Be aware that we use `train_test_split` here for didactic purposes, to show
-# the scikit-learn API. In a real setting one should use cross-validation as
-# previously demonstrated.
+# the scikit-learn API. In a real setting one might prefer to use
+# cross-validation to also be able to evaluate the uncertainty of
+# our estimation of the generalization performance of a model,
+# as previously demonstrated.
 # ```
 #
 # Now, we can train the model on the train set.

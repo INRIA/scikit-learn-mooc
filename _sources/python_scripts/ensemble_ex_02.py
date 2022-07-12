6958cfeaba0d04ebf74523005a338320a09b0943
lesteve <lesteve@users.noreply.github.com>
2022-07-12 14:46:31 +0000

    [ci skip] Split instructions in Exercise M6.02 (#644)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Loïc Estève <loic.esteve@ymail.com> 1124263857638b1dc8b49d1f42827ea9511e5b16

--- _sources/python_scripts/ensemble_ex_02.py
+++ _sources/python_scripts/ensemble_ex_02.py
@@ -46,19 +46,28 @@ data_train, data_test, target_train, target_test = train_test_split(
 # Write your code here.
 
 # %% [markdown]
-# The next steps of this exercise are to:
-#
-# - create a new dataset containing the penguins with a flipper length
-#   between 170 mm and 230 mm;
-# - plot the training data using a scatter plot;
-# - plot the decision of each individual tree by predicting on the newly
-#   created dataset;
-# - plot the decision of the random forest using this newly created dataset.
-#
-# ```{tip}
-# The trees contained in the forest that you created can be accessed
-# with the attribute `estimators_`.
-# ```
+# We now aim to plot the predictions from the individual trees in the forest.
+# For that purpose you have to create first a new dataset containing evenly
+# spaced values for the flipper length over the interval between 170 mm and 230
+# mm.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# The trees contained in the forest that you created can be accessed with the
+# attribute `estimators_`. Use them to predict the body mass corresponding to
+# the values in this newly created dataset. Similarly find the predictions of
+# the random forest in this dataset.
+
+# %%
+# Write your code here.
+
+# %% [markdown]
+# Now make a plot that displays:
+# - the whole `data` using a scatter plot;
+# - the decision of each individual tree;
+# - the decision of the random forest.
 
 # %%
 # Write your code here.

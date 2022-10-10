6341a630e53df3fa8879e9754a32b89d295ec3ca
Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>
2022-10-10 11:53:56 +0200

    Make optimal pipeline in Exercise M3.02 depend on preprocessing strategy (#662)

--- python_scripts/parameter_tuning_sol_03.py
+++ python_scripts/parameter_tuning_sol_03.py
@@ -10,116 +10,78 @@
 #
 # The goal is to find the best set of hyperparameters which maximize the
 # generalization performance on a training set.
-#
-# Here again with limit the size of the training set to make computation
-# run faster. Feel free to increase the `train_size` value if your computer
-# is powerful enough.
 
 # %%
-
-import numpy as np
-import pandas as pd
-
-adult_census = pd.read_csv("../datasets/adult-census.csv")
-
-target_name = "class"
-target = adult_census[target_name]
-data = adult_census.drop(columns=[target_name, "education-num"])
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
+data, target = fetch_california_housing(return_X_y=True, as_frame=True)
+target *= 100  # rescale the target in k$
+
 data_train, data_test, target_train, target_test = train_test_split(
-    data, target, train_size=0.2, random_state=42)
+    data, target, random_state=42)
 
 # %% [markdown]
-# In this exercise, we will progressively define the classification pipeline
+# In this exercise, we will progressively define the regression pipeline
 # and later tune its hyperparameters.
 #
-# Our pipeline should:
-# * preprocess the categorical columns using a `OneHotEncoder` and use a
-#   `StandardScaler` to normalize the numerical data.
-# * use a `LogisticRegression` as a predictive model.
-#
-# Start by defining the columns and the preprocessing pipelines to be applied
-# on each group of columns.
-# %%
-from sklearn.compose import make_column_selector as selector
+# Start by defining a pipeline that:
+# * uses a `StandardScaler` to normalize the numerical data;
+# * uses a `sklearn.neighbors.KNeighborsRegressor` as a predictive model.
 
+# %%
 # solution
-categorical_columns_selector = selector(dtype_include=object)
-categorical_columns = categorical_columns_selector(data)
-
-numerical_columns_selector = selector(dtype_exclude=object)
-numerical_columns = numerical_columns_selector(data)
-
-# %%
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-
-# solution
-categorical_processor = OneHotEncoder(handle_unknown="ignore")
-numerical_processor = StandardScaler()
-
-# %% [markdown]
-# Subsequently, create a `ColumnTransformer` to redirect the specific columns
-# a preprocessing pipeline.
-
-# %%
-from sklearn.compose import ColumnTransformer
-
-# solution
-preprocessor = ColumnTransformer(
-    [('cat_preprocessor', categorical_processor, categorical_columns),
-     ('num_preprocessor', numerical_processor, numerical_columns)]
-)
-
-# %% [markdown]
-# Assemble the final pipeline by combining the above preprocessor
-# with a logistic regression classifier. Force the maximum number of
-# iterations to `10_000` to ensure that the model will converge.
-
-# %%
 from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
 
-# solution
-model = make_pipeline(preprocessor, LogisticRegression(max_iter=10_000))
+scaler = StandardScaler()
+model = make_pipeline(scaler, KNeighborsRegressor())
 
 # %% [markdown]
 # Use `RandomizedSearchCV` with `n_iter=20` to find the best set of
 # hyperparameters by tuning the following parameters of the `model`:
 #
-# - the parameter `C` of the `LogisticRegression` with values ranging from
-#   0.001 to 10. You can use a log-uniform distribution
-#   (i.e. `scipy.stats.loguniform`);
+# - the parameter `n_neighbors` of the `KNeighborsRegressor` with values
+#   `np.logspace(0, 3, num=10).astype(np.int32)`;
 # - the parameter `with_mean` of the `StandardScaler` with possible values
 #   `True` or `False`;
 # - the parameter `with_std` of the `StandardScaler` with possible values
 #   `True` or `False`.
 #
+# Notice that in the notebook "Hyperparameter tuning by randomized-search" we
+# pass distributions to be sampled by the `RandomizedSearchCV`. In this case we
+# define a fixed grid of hyperparameters to be explored. Using a `GridSearchCV`
+# instead would explore all the possible combinations on the grid, which can be
+# costly to compute for large grids, whereas the parameter `n_iter` of the
+# `RandomizedSearchCV` controls the number of different random combination that
+# are evaluated. Notice that setting `n_iter` larger than the number of possible
+# combinations in a grid (in this case 10 x 2 x 2 = 40) would lead to repeating
+# already-explored combinations.
+#
 # Once the computation has completed, print the best combination of parameters
 # stored in the `best_params_` attribute.
 
 # %%
-from sklearn.model_selection import RandomizedSearchCV
-from scipy.stats import loguniform
-
 # solution
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+
 param_distributions = {
-    "logisticregression__C": loguniform(0.001, 10),
-    "columntransformer__num_preprocessor__with_mean": [True, False],
-    "columntransformer__num_preprocessor__with_std": [True, False],
+    "kneighborsregressor__n_neighbors": np.logspace(0, 3, num=10).astype(np.int32),
+    "standardscaler__with_mean": [True, False],
+    "standardscaler__with_std": [True, False],
 }
 
 model_random_search = RandomizedSearchCV(
     model, param_distributions=param_distributions,
-    n_iter=20, error_score=np.nan, n_jobs=2, verbose=1, random_state=1)
+    n_iter=20, n_jobs=2, verbose=1, random_state=1)
 model_random_search.fit(data_train, target_train)
 model_random_search.best_params_
 
 # %% [markdown] tags=["solution"]
-#
-# So the best hyperparameters give a model where the features are scaled but
-# not centered and the final model is regularized.
+# So the best hyperparameters give a model where the features are scaled but not
+# centered.
 #
 # Getting the best parameter combinations is the main outcome of the
 # hyper-parameter optimization procedure. However it is also interesting to
@@ -130,12 +92,12 @@ model_random_search.best_params_
 #
 # We could use `cv_results = model_random_search.cv_results_` to make a
 # parallel coordinate plot as we did in the previous notebook (you are more
-# than welcome to try!). Instead we are going to load the results obtained from
-# a similar search with many more iterations (1,000 instead of 20).
+# than welcome to try!).
 
 # %% tags=["solution"]
-cv_results = pd.read_csv(
-    "../figures/randomized_search_results_logistic_regression.csv")
+import pandas as pd
+
+cv_results = pd.DataFrame(model_random_search.cv_results_)
 
 # %% [markdown] tags=["solution"]
 # To simplify the axis of the plot, we will rename the column of the dataframe
@@ -143,37 +105,36 @@ cv_results = pd.read_csv(
 
 # %% tags=["solution"]
 column_name_mapping = {
-    "param_logisticregression__C": "C",
-    "param_columntransformer__num_preprocessor__with_mean": "centering",
-    "param_columntransformer__num_preprocessor__with_std": "scaling",
-    "mean_test_score": "mean test accuracy",
+    "param_kneighborsregressor__n_neighbors": "n_neighbors",
+    "param_standardscaler__with_mean": "centering",
+    "param_standardscaler__with_std": "scaling",
+    "mean_test_score": "mean test score",
 }
 
 cv_results = cv_results.rename(columns=column_name_mapping)
 cv_results = cv_results[column_name_mapping.values()].sort_values(
-    "mean test accuracy", ascending=False)
+    "mean test score", ascending=False)
 
 # %% [markdown] tags=["solution"]
-# In addition, the parallel coordinate plot from `plotly` expects all data to
-# be numeric. Thus, we convert the boolean indicator informing whether or not
-# the data were centered or scaled into an integer, where True is mapped to 1
-# and False is mapped to 0.
-#
-# We also take the logarithm of the `C` values to span the data on a broader
-# range for a better visualization.
+# In addition, the parallel coordinate plot from `plotly` expects all data to be
+# numeric. Thus, we convert the boolean indicator informing whether or not the
+# data were centered or scaled into an integer, where True is mapped to 1 and
+# False is mapped to 0. As `n_neighbors` has `dtype=object`, we also convert it
+# explicitly to an integer.
 
 # %% tags=["solution"]
 column_scaler = ["centering", "scaling"]
 cv_results[column_scaler] = cv_results[column_scaler].astype(np.int64)
-cv_results['log C'] = np.log10(cv_results['C'])
+cv_results["n_neighbors"] = cv_results["n_neighbors"].astype(np.int64)
+cv_results
 
 # %% tags=["solution"]
 import plotly.express as px
 
 fig = px.parallel_coordinates(
     cv_results,
-    color="mean test accuracy",
-    dimensions=["log C", "centering", "scaling", "mean test accuracy"],
+    color="mean test score",
+    dimensions=["n_neighbors", "centering", "scaling", "mean test score"],
     color_continuous_scale=px.colors.diverging.Tealrose,
 )
 fig.show()
@@ -183,14 +144,34 @@ fig.show()
 # holding on any axis of the parallel coordinate plot. You can then slide
 # (move) the range selection and cross two selections to see the intersections.
 #
-# Selecting the best performing models (i.e. above an accuracy of ~0.845), we
-# observe the following pattern:
+# Selecting the best performing models (i.e. above an accuracy of ~0.68), we
+# observe that **in this case**:
 #
-# - scaling the data is important. All the best performing models are scaling
-#   the data;
-# - centering the data does not have a strong impact. Both approaches,
-#   centering and not centering, can lead to good models;
-# - using some regularization is fine but using too much is a problem. Recall
-#   that a smaller value of C means a stronger regularization. In particular
-#   no pipeline with C lower than 0.001 can be found among the best
-#   models.
+# - scaling the data is important. All the best performing models use scaled
+#   features;
+# - centering the data does not have a strong impact. Both approaches, centering
+#   and not centering, can lead to good models;
+# - using some neighbors is fine but using too many is a problem. In particular
+#   no pipeline with `n_neighbors=1` can be found among the best models.
+#   However, scaling features has an even stronger impact than the choice of
+#   `n_neighbors` in this problem.
+#
+# The reason is that fitting scaled data leads to a completely different
+# KNeighbors model:  if you have two variables A and B where A has values which
+# vary between 0 and 10,000 (e.g. the variable `"Population"`) and B is a
+# feature that varies between 1 and 10 (e.g. the variable `"AveRooms"`), then
+# distances between samples (rows of the dataframe) are mostly impacted by
+# differences in values of the column A, while values of the column B will be
+# comparatively ignored. If one applies StandardScaler to such a database, both
+# the values of A and B will be approximately between -3 and 3 and the neighbor
+# structure will be impacted more or less equivalently by both variables.
+#
+# Note that **in this case** the models with scaled features perform better
+# than the models with non-scaled features because all the variables are
+# expected to be predictive and we rather avoid some of them being comparatively
+# ignored.
+#
+# If the variables in lower scales were not predictive one may experience a
+# decrease of the performance after scaling the features: noisy features would
+# contribute more to the prediction after scaling and therefore scaling would
+# increase overfitting.

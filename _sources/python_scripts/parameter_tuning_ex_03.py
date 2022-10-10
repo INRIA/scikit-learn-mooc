0819fdae7bb3f854159711bac3ec4ba4b46e9a4c
ArturoAmorQ <ArturoAmorQ@users.noreply.github.com>
2022-10-10 09:56:55 +0000

    [ci skip] Make optimal pipeline in Exercise M3.02 depend on preprocessing strategy (#662) 6341a630e53df3fa8879e9754a32b89d295ec3ca

--- _sources/python_scripts/parameter_tuning_ex_03.py
+++ _sources/python_scripts/parameter_tuning_ex_03.py
@@ -17,85 +17,51 @@
 #
 # The goal is to find the best set of hyperparameters which maximize the
 # generalization performance on a training set.
-#
-# Here again with limit the size of the training set to make computation
-# run faster. Feel free to increase the `train_size` value if your computer
-# is powerful enough.
 
 # %%
-
-import numpy as np
-import pandas as pd
-
-adult_census = pd.read_csv("../datasets/adult-census.csv")
-
-target_name = "class"
-target = adult_census[target_name]
-data = adult_census.drop(columns=[target_name, "education-num"])
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
+data, target = fetch_california_housing(return_X_y=True, as_frame=True)
+target *= 100  # rescale the target in k$
+
 data_train, data_test, target_train, target_test = train_test_split(
-    data, target, train_size=0.2, random_state=42)
+    data, target, random_state=42)
 
 # %% [markdown]
-# In this exercise, we will progressively define the classification pipeline
+# In this exercise, we will progressively define the regression pipeline
 # and later tune its hyperparameters.
 #
-# Our pipeline should:
-# * preprocess the categorical columns using a `OneHotEncoder` and use a
-#   `StandardScaler` to normalize the numerical data.
-# * use a `LogisticRegression` as a predictive model.
-#
-# Start by defining the columns and the preprocessing pipelines to be applied
-# on each group of columns.
-# %%
-from sklearn.compose import make_column_selector as selector
-
-# Write your code here.
+# Start by defining a pipeline that:
+# * uses a `StandardScaler` to normalize the numerical data;
+# * uses a `sklearn.neighbors.KNeighborsRegressor` as a predictive model.
 
 # %%
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-
-# Write your code here.
-
-# %% [markdown]
-# Subsequently, create a `ColumnTransformer` to redirect the specific columns
-# a preprocessing pipeline.
-
-# %%
-from sklearn.compose import ColumnTransformer
-
-# Write your code here.
-
-# %% [markdown]
-# Assemble the final pipeline by combining the above preprocessor
-# with a logistic regression classifier. Force the maximum number of
-# iterations to `10_000` to ensure that the model will converge.
-
-# %%
-from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LogisticRegression
-
 # Write your code here.
 
 # %% [markdown]
 # Use `RandomizedSearchCV` with `n_iter=20` to find the best set of
 # hyperparameters by tuning the following parameters of the `model`:
 #
-# - the parameter `C` of the `LogisticRegression` with values ranging from
-#   0.001 to 10. You can use a log-uniform distribution
-#   (i.e. `scipy.stats.loguniform`);
+# - the parameter `n_neighbors` of the `KNeighborsRegressor` with values
+#   `np.logspace(0, 3, num=10).astype(np.int32)`;
 # - the parameter `with_mean` of the `StandardScaler` with possible values
 #   `True` or `False`;
 # - the parameter `with_std` of the `StandardScaler` with possible values
 #   `True` or `False`.
 #
+# Notice that in the notebook "Hyperparameter tuning by randomized-search" we
+# pass distributions to be sampled by the `RandomizedSearchCV`. In this case we
+# define a fixed grid of hyperparameters to be explored. Using a `GridSearchCV`
+# instead would explore all the possible combinations on the grid, which can be
+# costly to compute for large grids, whereas the parameter `n_iter` of the
+# `RandomizedSearchCV` controls the number of different random combination that
+# are evaluated. Notice that setting `n_iter` larger than the number of possible
+# combinations in a grid (in this case 10 x 2 x 2 = 40) would lead to repeating
+# already-explored combinations.
+#
 # Once the computation has completed, print the best combination of parameters
 # stored in the `best_params_` attribute.
 
 # %%
-from sklearn.model_selection import RandomizedSearchCV
-from scipy.stats import loguniform
-
 # Write your code here.

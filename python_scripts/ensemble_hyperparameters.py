a18e9fd4bf215e612c85f71aa17c8cc1037ada81
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-10-10 17:00:12 +0200

    Avoid tunning n_estimators and tune max_features instead

--- python_scripts/ensemble_hyperparameters.py
+++ python_scripts/ensemble_hyperparameters.py
@@ -74,7 +74,7 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
 
 param_distributions = {
-    "n_estimators": [1, 2, 5, 10, 20, 50, 100, 200, 500],
+    "max_features": [1, 2, 3, 5, None],
     "max_leaf_nodes": [2, 5, 10, 20, 50, 100],
 }
 search_cv = RandomizedSearchCV(

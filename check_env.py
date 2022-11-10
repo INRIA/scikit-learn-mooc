e46fef7096f91f802da79bbea59e1c7f536aed9e
Loïc Estève <loic.esteve@ymail.com>
2022-11-10 16:30:41 +0100

    Remove deprecated distutils.Version in check_env.py (#672)
    Fix https://github.com/INRIA/scikit-learn-mooc/issues/549

--- check_env.py
+++ check_env.py
@@ -1,22 +1,35 @@
-from __future__ import print_function
-from distutils.version import LooseVersion as Version
 import sys
+import importlib
 
-OK = '\x1b[42m[ OK ]\x1b[0m'
+OK = "\x1b[42m[ OK ]\x1b[0m"
 FAIL = "\x1b[41m[FAIL]\x1b[0m"
 
 try:
-    import importlib
+    from packaging.version import Version
 except ImportError:
-    print(FAIL, "Python version 3.6 or above is required,"
-                " but %s is installed." % sys.version)
+    print(FAIL, "'packaging' package not installed, install it with conda or pip")
+    sys.exit(1)
+
+# first check the python version
+print("Using python in", sys.prefix)
+print(sys.version)
+pyversion_str = f"{sys.version_info.major}.{sys.version_info.minor}"
+pyversion = Version(pyversion_str)
+
+if pyversion < Version("3.8"):
+    print(
+        FAIL,
+        "Python version 3.8 or above is required," f" but {pyversion_str} is installed.",
+    )
+    sys.exit(1)
+print()
 
 
 def import_version(pkg, min_ver, fail_msg=""):
     mod = None
     try:
         mod = importlib.import_module(pkg)
-        if pkg in {'PIL'}:
+        if pkg in {"PIL"}:
             try:
                 ver = mod.__version__
             except AttributeError:
@@ -25,39 +38,32 @@ def import_version(pkg, min_ver, fail_msg=""):
                 except AttributeError:
                     try:
                         ver = mod.PILLOW_VERSION
-                    except:
+                    except Exception:
                         raise
         else:
             ver = mod.__version__
-        if Version(ver) < min_ver:
-            print(FAIL, "%s version %s or higher required, but %s installed."
-                  % (lib, min_ver, ver))
+        if Version(ver) < Version(min_ver):
+            print(
+                FAIL,
+                f"{lib} version {min_ver} or higher required, but {ver} installed.",
+            )
         else:
-            print(OK, '%s version %s' % (pkg, ver))
+            print(OK, f"{pkg} version {ver}")
     except ImportError:
-        print(FAIL, '%s not installed. %s' % (pkg, fail_msg))
+        print(FAIL, f"{pkg} not installed. {fail_msg}")
     return mod
 
 
-# first check the python version
-print('Using python in', sys.prefix)
-print(sys.version)
-pyversion = Version(sys.version)
-if pyversion >= "3":
-    if pyversion < "3.6":
-        print(FAIL, "Python version 3.6 or above is required,"
-                    " but %s is installed." % sys.version)
-elif pyversion >= "2":
-    print(FAIL, "Python version 3.6 or above is required,"
-                " but %s is installed." % sys.version)
-else:
-    print(FAIL, "Unknown Python version: %s" % sys.version)
-
-print()
-requirements = {'numpy': "1.16", 'scipy': "1.2", 'matplotlib': "3.0",
-                'sklearn': "1.1", 'pandas': "1",
-                'seaborn': "0.11",
-                'notebook': "5.7", 'plotly': "5.10"}
+requirements = {
+    "numpy": "1.16",
+    "scipy": "1.2",
+    "matplotlib": "3.0",
+    "sklearn": "1.1",
+    "pandas": "1",
+    "seaborn": "0.11",
+    "notebook": "5.7",
+    "plotly": "5.10",
+}
 
 # now the dependencies
 for lib, required_version in list(requirements.items()):

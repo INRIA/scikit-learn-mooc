0f885d847fcf9d56439ae6f24bbef22eac357b1f
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2023-06-12 17:51:17 +0200

    Add axis labels to plot about scaling

--- figures/numerical_pipeline_quiz_scaler.py
+++ figures/numerical_pipeline_quiz_scaler.py
@@ -9,6 +9,7 @@ X[:, 1] *= 3
 
 # %%
 import seaborn as sns
+
 sns.set_context("talk")
 
 # %%
@@ -21,17 +22,19 @@ _, ax = plt.subplots(figsize=(5, 5))
 plt.scatter(X[:, 0], X[:, 1], s=30, edgecolors="black")
 ax.set_xlim(-6, 6)
 ax.set_ylim(-6, 6)
-ax.spines['left'].set_position('center')
-ax.spines['right'].set_color('none')
-ax.spines['bottom'].set_position('center')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
+ax.set_xlabel("Feature A", position=(1.2, 0), labelpad=-30)
+ax.set_ylabel("Feature B\n", loc="top", rotation=0, labelpad=-70)
+ax.spines["left"].set_position("center")
+ax.spines["right"].set_color("none")
+ax.spines["bottom"].set_position("center")
+ax.spines["top"].set_color("none")
+ax.xaxis.set_ticks_position("bottom")
+ax.yaxis.set_ticks_position("left")
 ax.set_xticklabels(ticks)
 ax.xaxis.set_major_locator(ticker.FixedLocator(ticks))
 ax.set_yticklabels(ticks)
 ax.yaxis.set_major_locator(ticker.FixedLocator(ticks))
-ax.set_title("Original dataset\n")
+ax.set_title("Original dataset\n\n\n", loc="center")
 plt.savefig("numerical_pipeline_quiz_scaler_original.png")
 
 # %%
@@ -44,32 +47,36 @@ standard_scaler = StandardScaler().fit(X)
 min_max_scaler = MinMaxScaler().fit(X)
 
 # %%
-fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(10, 10))
-for idx, (ax, data) in enumerate(zip(
-    axs.ravel(),
-    [
-        standard_scaler_mean_only.transform(X),
-        standard_scaler.transform(X),
-        min_max_scaler.transform(X),
-        standard_scaler_scale_only.transform(X),
-    ]
-)):
+fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(12, 10))
+for idx, (ax, data) in enumerate(
+    zip(
+        axs.ravel(),
+        [
+            standard_scaler_mean_only.transform(X),
+            standard_scaler.transform(X),
+            min_max_scaler.transform(X),
+            standard_scaler_scale_only.transform(X),
+        ],
+    )
+):
     ax.scatter(data[:, 0], data[:, 1], s=30, edgecolors="black")
     ax.set_xlim(-6, 6)
     ax.set_ylim(-6, 6)
-    ax.spines['left'].set_position('center')
-    ax.spines['right'].set_color('none')
-    ax.spines['bottom'].set_position('center')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    ax.set_xlabel("Feature A", position=(1.2, 0), labelpad=-30)
+    ax.set_ylabel("Feature B\n", loc="top", rotation=0, labelpad=-70)
+    ax.spines["left"].set_position("center")
+    ax.spines["right"].set_color("none")
+    ax.spines["bottom"].set_position("center")
+    ax.spines["top"].set_color("none")
+    ax.xaxis.set_ticks_position("bottom")
+    ax.yaxis.set_ticks_position("left")
     ax.set_xticklabels(ticks)
     ax.xaxis.set_major_locator(ticker.FixedLocator(ticks))
     ax.set_yticklabels(ticks)
     ax.yaxis.set_major_locator(ticker.FixedLocator(ticks))
-    ax.set_title(f"Preprocessing {'ABCD'[idx]}\n")
+    ax.set_title(f"Preprocessing {'ABCD'[idx]}\n\n\n")
 
-fig.subplots_adjust(hspace=0.35)
+fig.subplots_adjust(hspace=0.5, wspace=0.5)
 plt.savefig("numerical_pipeline_quiz_scaler_preprocessing.png")
 
 # %%

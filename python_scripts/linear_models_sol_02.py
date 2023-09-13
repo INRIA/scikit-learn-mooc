2077f6ba5a3df6729f40eae2ee3adcc1dd02f007
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2023-09-13 16:18:47 +0200

    ENH Expand contents related to Nystroem

--- python_scripts/linear_models_sol_02.py
+++ python_scripts/linear_models_sol_02.py
@@ -9,8 +9,8 @@
 # # 📃 Solution for Exercise M4.02
 #
 # In the previous notebook, we showed that we can add new features based on the
-# original feature to make the model more expressive, for instance `x ** 2` or `x ** 3`.
-# In that case we only used a single feature in `data`.
+# original feature to make the model more expressive, for instance `x ** 2` or
+# `x ** 3`. In that case we only used a single feature in `data`.
 #
 # The aim of this notebook is to train a linear regression algorithm on a
 # dataset with more than a single feature. In such a "multi-dimensional" feature
@@ -76,7 +76,9 @@ cv_results = cross_validate(
 )
 
 # %% [markdown]
-# Compute the mean and std of the MAE in grams (g).
+# Compute the mean and std of the MAE in grams (g). Remember you have to revert
+# the sign introduced when metrics start with `neg_`, such as in
+# `"neg_mean_absolute_error"`.
 
 # %%
 # solution
@@ -94,7 +96,8 @@ print(
 # introducing a redundancy with the intercept of the subsequent linear
 # regression model.
 #
-# Use the same strategy as before to cross-validate such a pipeline.
+# You may want to use the `.set_output(transform="pandas")` method of the
+# pipeline to answer the next question.
 
 # %%
 # solution
@@ -106,8 +109,36 @@ poly_features = PolynomialFeatures(
 )
 linear_regression_interactions = make_pipeline(
     poly_features, linear_regression
-)
+).set_output(transform="pandas")
 
+# %% [markdown]
+# Transform the first 5 rows of the dataset and look at the column names. How
+# many features are generated at the output of the `PolynomialFeatures` step in
+# the previous pipeline? Check that the values for the new interactions features
+# are correct for a few of them.
+
+# %%
+# solution
+linear_regression_interactions.fit(data, target)
+linear_regression_interactions[0].transform(data[:5])
+
+# %% [markdown] tags=["solution"]
+# We observe that 3 features are generated, corresponding to the different
+# combinations of products of the 3 original features.
+
+# %%
+# solution
+culmen_length_first_sample = 181.0
+culmen_depth_first_sample = 18.7
+culmen_length_first_sample * culmen_depth_first_sample
+
+# %% [markdown]
+# Use the same cross-validation strategy as done previously to estimate the mean
+# and std of the MAE in grams (g) for such a pipeline. Compare with the results
+# without feature engineering.
+
+# %%
+# solution
 cv_results = cross_validate(
     linear_regression_interactions,
     data,
@@ -116,13 +147,6 @@ cv_results = cross_validate(
     scoring="neg_mean_absolute_error",
     n_jobs=2,
 )
-
-# %% [markdown]
-# Compute the mean and std of the MAE in grams (g) and compare with the results
-# without feature engineering.
-
-# %%
-# solution
 print(
     "Mean absolute error on testing set with interactions: "
     f"{-cv_results['test_score'].mean():.3f} ± "
@@ -130,7 +154,66 @@ print(
 )
 
 # %% [markdown] tags=["solution"]
-# We observe that the mean absolute error is lower and less spread with the
-# enriched features. In this case the "interactions" are indeed predictive. In
-# the following notebook we will see what happens when the enriched features are
-# non-predictive and how to deal with this case.
+# We observe that the MAE is lower and less spread with the enriched features.
+# In this case the "interactions" are indeed predictive. Later in this module we
+# will see what happens when the enriched features are non-predictive and how to
+# deal with this case.
+
+# %% [markdown]
+# Now let's try to see if we can build an alternatively pipeline with fewer
+# intermediate features while keeping a similar predictive power. To do so,
+# try using the `Nystroem` transformer instead of `PolynomialFeatures`. Set
+# the kernel parameter to `"poly"` and `degree` to 2. Adjust the number of
+# components to be as small as possible while keeping a good cross-validation
+# performance.
+#
+# Hint: Use a `ValidationCurveDisplay` with `param_range = np.array([1, 2, 5,
+# 10, 20, 50, 100])` to find the optimal `n_components`.
+
+# %%
+# solution
+import numpy as np
+
+from sklearn.kernel_approximation import Nystroem
+from sklearn.model_selection import ValidationCurveDisplay
+
+nystroem_regression = make_pipeline(
+    Nystroem(kernel="poly", degree=2, random_state=0),
+    linear_regression,
+)
+
+param_range = np.array([1, 2, 5, 10, 20, 50, 100])
+disp = ValidationCurveDisplay.from_estimator(
+    nystroem_regression,
+    data,
+    target,
+    param_name="nystroem__n_components",
+    param_range=param_range,
+    cv=10,
+    scoring="neg_mean_absolute_error",
+    negate_score=True,
+    std_display_style="errorbar",
+    n_jobs=2,
+)
+
+_ = disp.ax_.set(
+    xlabel="Number of components",
+    ylabel="Mean absolute error (g)",
+    title="Validation curve for Nystroem regression",
+)
+# %%
+# solution
+nystroem_regression.set_params(nystroem__n_components=10)
+cv_results = cross_validate(
+    nystroem_regression,
+    data,
+    target,
+    cv=10,
+    scoring="neg_mean_absolute_error",
+    n_jobs=2,
+)
+print(
+    "Mean absolute error on testing set with nystroem: "
+    f"{-cv_results['test_score'].mean():.3f} ± "
+    f"{cv_results['test_score'].std():.3f} g"
+)

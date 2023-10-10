12c3f33844ea967dab4b14e46857ff7053e28ef5
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2023-10-10 11:29:22 +0200

    Improve text and prefer verbs in present mode

--- python_scripts/trees_classification.py
+++ python_scripts/trees_classification.py
@@ -8,8 +8,8 @@
 # %% [markdown]
 # # Build a classification decision tree
 #
-# We will illustrate how decision tree fit data with a simple classification
-# problem using the penguins dataset.
+# In this notebook we illustrate decision trees in a multiclass classification
+# problem by using the penguins dataset with 2 features and 3 classes.
 
 # %% [markdown]
 # ```{note}
@@ -25,8 +25,8 @@ culmen_columns = ["Culmen Length (mm)", "Culmen Depth (mm)"]
 target_column = "Species"
 
 # %% [markdown]
-# Besides, we split the data into two subsets to investigate how trees will
-# predict values based on an out-of-samples dataset.
+# First, we split the data into two subsets to investigate how trees predict
+# values based on unseen data.
 
 # %%
 from sklearn.model_selection import train_test_split
@@ -37,16 +37,13 @@ data_train, data_test, target_train, target_test = train_test_split(
 )
 
 # %% [markdown]
-# In a previous notebook, we learnt that a linear classifier will define a
-# linear separation to split classes using a linear combination of the input
-# features. In our 2-dimensional space, it means that a linear classifier will
-# define some oblique lines that best separate our classes. We define a function
-# below that, given a set of data points and a classifier, will plot the
-# decision boundaries learnt by the classifier.
-#
-# Thus, for a linear classifier, we will obtain the following decision
-# boundaries. These boundaries lines indicate where the model changes its
-# prediction from one class to another.
+# In a previous notebook, we learnt that linear classifiers define a linear
+# separation to split classes using a linear combination of the input features.
+# In our 2-dimensional feature space, it means that a linear classifier finds
+# the oblique lines that best separate the classes. This is still true for
+# multiclass problems, except that more than one line is fitted. We can use
+# `DecisionBoundaryDisplay` to plot the decision boundaries learnt by the
+# classifier.
 
 # %%
 from sklearn.linear_model import LogisticRegression
@@ -80,7 +77,7 @@ _ = plt.title("Decision boundary using a logistic regression")
 # %% [markdown]
 # We see that the lines are a combination of the input features since they are
 # not perpendicular a specific axis. Indeed, this is due to the model
-# parametrization that we saw in the previous notebook, controlled by the
+# parametrization that we saw in some previous notebooks, i.e. controlled by the
 # model's weights and intercept.
 #
 # Besides, it seems that the linear model would be a good candidate for such
@@ -93,12 +90,12 @@ print(f"Accuracy of the LogisticRegression: {test_score:.2f}")
 
 # %% [markdown]
 # Unlike linear models, decision trees are non-parametric models: they are not
-# controlled by a mathematical decision function and do not have weights or
+# controlled by a mathematical decision function and do not have weights or an
 # intercept to be optimized.
 #
-# Indeed, decision trees will partition the space by considering a single
-# feature at a time. Let's illustrate this behaviour by having a decision tree
-# make a single split to partition the feature space.
+# Indeed, decision trees partition the space by considering a single feature at
+# a time. Let's illustrate this behaviour by having a decision tree make a
+# single split to partition the feature space.
 
 # %%
 from sklearn.tree import DecisionTreeClassifier
@@ -123,7 +120,7 @@ _ = plt.title("Decision boundary using a decision tree")
 # %% [markdown]
 # The partitions found by the algorithm separates the data along the axis
 # "Culmen Depth", discarding the feature "Culmen Length". Thus, it highlights
-# that a decision tree does not use a combination of feature when making a
+# that a decision tree does not use a combination of features when making a
 # split. We can look more in depth at the tree structure.
 
 # %%
@@ -150,36 +147,40 @@ _ = plot_tree(
 # dataset was subdivided into 2 sets based on the culmen depth (inferior or
 # superior to 16.45 mm).
 #
-# This partition of the dataset minimizes the class diversities in each
+# This partition of the dataset minimizes the class diversity in each
 # sub-partitions. This measure is also known as a **criterion**, and is a
 # settable parameter.
 #
 # If we look more closely at the partition, we see that the sample superior to
-# 16.45 belongs mainly to the Adelie class. Looking at the values, we indeed
-# observe 103 Adelie individuals in this space. We also count 52 Chinstrap
-# samples and 6 Gentoo samples. We can make similar interpretation for the
+# 16.45 belongs mainly to the "Adelie" class. Looking at the values, we indeed
+# observe 103 "Adelie" individuals in this space. We also count 52 "Chinstrap"
+# samples and 6 "Gentoo" samples. We can make similar interpretation for the
 # partition defined by a threshold inferior to 16.45mm. In this case, the most
-# represented class is the Gentoo species.
+# represented class is the "Gentoo" species.
 #
 # Let's see how our tree would work as a predictor. Let's start with a case
 # where the culmen depth is inferior to the threshold.
 
 # %%
-sample_1 = pd.DataFrame({"Culmen Length (mm)": [0], "Culmen Depth (mm)": [15]})
-tree.predict(sample_1)
+test_penguin_1 = pd.DataFrame(
+    {"Culmen Length (mm)": [0], "Culmen Depth (mm)": [15]}
+)
+tree.predict(test_penguin_1)
 
 # %% [markdown]
-# The class predicted is the Gentoo. We can now check what happens if we pass a
+# The class predicted is the "Gentoo". We can now check what happens if we pass a
 # culmen depth superior to the threshold.
 
 # %%
-sample_2 = pd.DataFrame({"Culmen Length (mm)": [0], "Culmen Depth (mm)": [17]})
-tree.predict(sample_2)
+test_penguin_2 = pd.DataFrame(
+    {"Culmen Length (mm)": [0], "Culmen Depth (mm)": [17]}
+)
+tree.predict(test_penguin_2)
 
 # %% [markdown]
-# In this case, the tree predicts the Adelie specie.
+# In this case, the tree predicts the "Adelie" specie.
 #
-# Thus, we can conclude that a decision tree classifier will predict the most
+# Thus, we can conclude that a decision tree classifier predicts the most
 # represented class within a partition.
 #
 # During the training, we have a count of samples in each partition, we can also
@@ -187,7 +188,7 @@ tree.predict(sample_2)
 # partition.
 
 # %%
-y_pred_proba = tree.predict_proba(sample_2)
+y_pred_proba = tree.predict_proba(test_penguin_2)
 y_proba_class_0 = pd.Series(y_pred_proba[0], index=tree.classes_)
 
 # %%
@@ -212,14 +213,14 @@ print(
 
 # %% [markdown]
 # It is also important to note that the culmen length has been disregarded for
-# the moment. It means that whatever the value given, it will not be used during
+# the moment. It means that regardless of its value, it is not be used during
 # the prediction.
 
 # %%
-sample_3 = pd.DataFrame(
+test_penguin_3 = pd.DataFrame(
     {"Culmen Length (mm)": [10_000], "Culmen Depth (mm)": [17]}
 )
-tree.predict_proba(sample_3)
+tree.predict_proba(test_penguin_3)
 
 # %% [markdown]
 # Going back to our classification problem, the split found with a maximum depth
@@ -232,9 +233,10 @@ test_score = tree.score(data_test, target_test)
 print(f"Accuracy of the DecisionTreeClassifier: {test_score:.2f}")
 
 # %% [markdown]
-# Indeed, it is not a surprise. We saw earlier that a single feature will not be
-# able to separate all three species. However, from the previous analysis we saw
-# that by using both features we should be able to get fairly good results.
+# Indeed, it is not a surprise. We saw earlier that a single feature is not able
+# to separate all three species: it underfits. However, from the previous
+# analysis we saw that by using both features we should be able to get fairly
+# good results.
 #
-# In the next exercise, you will increase the size of the tree depth. You will
-# get intuitions on how the space partitioning is repeated over time.
+# In the next exercise, you will increase the tree depth to get an intuition on
+# how such parameter affects the space partitioning.

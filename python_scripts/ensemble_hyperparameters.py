8ddaa2d5de15a264ecc3689762fd9d7155caa15a
Frits Hermans <post@fritshermans.nl>
2025-01-29 11:38:53 +0100

    Update python_scripts/ensemble_hyperparameters.py
    Co-authored-by: Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>

--- python_scripts/ensemble_hyperparameters.py
+++ python_scripts/ensemble_hyperparameters.py
@@ -164,8 +164,8 @@ print(
 # final prediction. A smaller learning-rate means the corrections of a new
 # tree result in small adjustments to the model prediction. When the
 # learning-rate is small, the model generally needs more trees to achieve good
-# performance. A higher learning-rate makes larger adjustments with each tree.
-# The model will need fewer trees and the model will train faster. The
+# performance. A higher learning-rate makes larger adjustments with each tree,
+# which requires fewer trees and trains faster, at the risk of overfitting. The
 # learning-rate needs to be tuned by hyperparameter tuning to obtain the best
 # value that results in a model with good generalization performance.
 

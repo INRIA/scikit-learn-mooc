39b6bb57df33b882a1ad847c40755effe252f540
Sylvain Combettes <48064216+sylvaincom@users.noreply.github.com>
2025-02-27 21:28:31 +0100

    Fix minor typo in `datasets_california_housing.py` (#803)

--- python_scripts/datasets_california_housing.py
+++ python_scripts/datasets_california_housing.py
@@ -106,9 +106,9 @@ california_housing.frame[features_of_interest].describe()
 # huge difference. It confirms the intuitions that there are a couple of extreme
 # values.
 #
-# Up to know, we discarded the longitude and latitude that carry geographical
-# information. In short, the combination of this feature could help us to decide
-# if there are locations associated with high-valued houses. Indeed, we could
+# Up to now, we discarded the longitude and latitude that carry geographical
+# information. In short, the combination of these features could help us decide
+# if there are locations associated with high-value houses. Indeed, we could
 # make a scatter plot where the x- and y-axis would be the latitude and
 # longitude and the circle size and color would be linked with the house value
 # in the district.

e86ccd51b41870045764c0a970f98b867de3c576
Olivier Grisel <olivier.grisel@ensta.org>
2025-04-03 11:41:44 +0200

    Update python_scripts/metrics_ex_01.py
    Co-authored-by: Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>

--- python_scripts/metrics_ex_01.py
+++ python_scripts/metrics_ex_01.py
@@ -70,7 +70,7 @@ from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 try:
     scores = cross_val_score(
-        tree, data, target, cv=10, scoring="precision", error_on_score="raise"
+        tree, data, target, cv=10, scoring="precision", error_score="raise"
     )
 except ValueError as exc:
     print(exc)

986934820abc6d3c0d793fdaa5dbb89271bb5590
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2025-04-03 16:59:34 +0200

    Prefer verbs in present form

--- python_scripts/metrics_ex_01.py
+++ python_scripts/metrics_ex_01.py
@@ -18,7 +18,7 @@
 # However, we did not use it with a cross-validation. This exercise aims at
 # practicing and implementing cross-validation.
 #
-# We will reuse the blood transfusion dataset.
+# Here we use the blood transfusion dataset.
 
 # %%
 import pandas as pd
@@ -41,7 +41,7 @@ target = blood_transfusion["Class"]
 
 # %% [markdown]
 # Create a `StratifiedKFold` cross-validation object. Then use it inside the
-# `cross_val_score` function to evaluate the decision tree. We will first use
+# `cross_val_score` function to evaluate the decision tree. We first use
 # the accuracy as a score function. Explicitly use the `scoring` parameter of
 # `cross_val_score` to compute the accuracy (even if this is the default score).
 # Check its documentation to learn how to do that.
@@ -56,12 +56,12 @@ target = blood_transfusion["Class"]
 # Write your code here.
 
 # %% [markdown]
-# We will now add a bit of complexity. We would like to compute the precision of
+# We now add a bit of complexity. We would like to compute the precision of
 # our model. However, during the course we saw that we need to mention the
 # positive label which in our case we consider to be the class `donated`.
 #
-# We will show that computing the precision without providing the positive label
-# will not be supported by scikit-learn because it is indeed ambiguous.
+# We can show that computing the precision without providing the positive label
+# is not supported by scikit-learn because it is indeed ambiguous.
 
 # %%
 from sklearn.model_selection import cross_val_score
@@ -105,10 +105,9 @@ except ValueError as exc:
 # Write your code here.
 
 # %% [markdown]
-# `cross_val_score` will only compute a single score provided to the `scoring`
-# parameter. The function `cross_validate` allows the computation of multiple
-# scores by passing a list of string or scorer to the parameter `scoring`, which
-# could be handy.
+# `cross_val_score` can compute one score at a time, as specified by the scoring
+# parameter. In contrast, cross_validate can compute multiple scores by passing
+# a list of strings or scorers to the scoring parameter
 #
 # Import `sklearn.model_selection.cross_validate` and compute the accuracy and
 # balanced accuracy through cross-validation. Plot the cross-validation score
